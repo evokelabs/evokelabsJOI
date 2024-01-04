a3e7ffb670da8d34bd62f62be70d8c70789ee437
Adrian Pikios <adrian@evokelabs.com>
2024-01-04 11:15:50 +1100

    limited orbittedcontrols

--- app/3d/EvokeLabs3D.tsx
+++ app/3d/EvokeLabs3D.tsx
@@ -5,9 +5,18 @@ import CyberpunkMap from './models/CyberpunkMap'
 
 const Evokelabs3D = () => {
   return (
-    <Canvas camera={{ position: [0, 1.5, -1], fov: 30, near: 1, far: 200 }} shadows>
+    <Canvas camera={{ position: [0, 1.5, -1], fov: 25, near: 1, far: 200 }} shadows>
       <Perf position='top-right' />
-      <OrbitControls makeDefault target={[-0, 1.2, 2.5]} />
+      <OrbitControls
+        makeDefault
+        target={[-0.25, 1.2, 2.5]}
+        enableZoom={false}
+        enablePan={false}
+        minAzimuthAngle={2.75}
+        maxAzimuthAngle={3.5}
+        minPolarAngle={1.2}
+        maxPolarAngle={1.8}
+      />
 
       <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} shadow-normalBias={0.04} />
       <ambientLight intensity={1} />
