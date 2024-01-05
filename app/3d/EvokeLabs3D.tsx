c943966e3ce0986546f6e27c66af6774a1120ce8
Adrian Pikios <adrian@evokelabs.com>
2024-01-05 13:12:33 +1100

    git cleanup + 3d map updated

--- app/3d/EvokeLabs3D.tsx
+++ app/3d/EvokeLabs3D.tsx
@@ -1,14 +1,15 @@
-// Importing necessary libraries and components
+import * as THREE from 'three'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
-import { OrbitControls, TransformControls } from '@react-three/drei'
+import { Environment, OrbitControls, useHelper } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { PerspectiveCamera, Vector3 } from 'three'
+import { PerspectiveCamera, Vector3, DirectionalLight } from 'three'
 import CyberpunkMap from './models/CyberpunkMap'
 import { getFov } from '../libs/helpers'
+import { useRef } from 'react'
 
 const debug = true
+// const debug = false
 
-// CameraRig component
 function CameraRig() {
   const { camera, size } = useThree()
 
@@ -34,17 +35,24 @@ function CameraRig() {
   return null
 }
 
-// Main component
+function DirectionalLightWithHelper() {
+  const lightRef = useRef<any>(null)
+
+  useHelper(lightRef, THREE.DirectionalLightHelper, 0.5, 'hotpink')
+
+  return <directionalLight ref={lightRef} position={[5, 2, 5]} intensity={5.5} shadow-normalBias={0.04} castShadow />
+}
 const Evokelabs3D = () => {
-  const fov = typeof window !== 'undefined' ? getFov(window.innerWidth) : 20 // Default to 20 if window object does not exist
+  const fov = typeof window !== 'undefined' ? getFov(window.innerWidth) : 50
 
   return (
-    <Canvas camera={{ position: [0, 1.5, -1], fov: fov, near: 1, far: 200 }} shadows>
+    <Canvas camera={{ position: [0, 1.5, -1], fov: fov, near: 0.1, far: 200 }} shadows>
+      <Environment background files={['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png']} path='/textures/cubeMap/' />
       <Perf position='top-right' />
       <OrbitControls makeDefault target={new Vector3(-0.2, 1.4, 2.5)} enableZoom={debug} enablePan={debug} enableRotate={debug} />
-      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} shadow-normalBias={0.04} />
-      <ambientLight intensity={1} />
-      <TransformControls />
+
+      {/* <DirectionalLightWithHelper /> */}
+      {/* <ambientLight intensity={0.75} color={'#005068'} /> */}
       <CyberpunkMap />
       {!debug && <CameraRig />}
     </Canvas>
--- app/3d/models/CyberpunkMap.tsx
+++ app/3d/models/CyberpunkMap.tsx
@@ -1,9 +1,32 @@
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/Addons.js'
+import { useEffect } from 'react'
+import { useLoader, useThree } from '@react-three/fiber'
+import { DRACOLoader, GLTFLoader } from 'three/examples/jsm/Addons.js'
+
+const DRACO_DECODER_PATH = 'https://www.gstatic.com/draco/versioned/decoders/1.5.6/'
 
 const CyberpunkMap = () => {
-  const gltf = useLoader(GLTFLoader, '/glb/EvokelabsRoom.glb')
-  return <primitive object={gltf.scene} />
+  const { scene } = useThree()
+
+  const dracoLoader = new DRACOLoader()
+  dracoLoader.setDecoderPath(DRACO_DECODER_PATH)
+
+  const gltfLoader = new GLTFLoader()
+  gltfLoader.setDRACOLoader(dracoLoader)
+
+  const gltf = useLoader(GLTFLoader, '/glb/EvokelabsRoom.glb', loader => loader.setDRACOLoader(dracoLoader))
+
+  useEffect(() => {
+    if (gltf.scene) {
+      scene.add(gltf.scene)
+    }
+    return () => {
+      if (gltf.scene) {
+        scene.remove(gltf.scene)
+      }
+    }
+  }, [gltf, scene])
+
+  return null
 }
 
 export default CyberpunkMap
