683108594d294e48581000990850e607e02e6368
Adrian Pikios <adrian@evokelabs.com>
2024-01-09 00:39:34 +1100

    Update CyberpunkCar animation and position

--- app/3d/models/CyberpunkCar.tsx
+++ app/3d/models/CyberpunkCar.tsx
@@ -1,15 +1,15 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useLoader, useThree, useFrame } from '@react-three/fiber'
 import { DRACOLoader, GLTFLoader } from 'three/examples/jsm/Addons.js'
 import { Object3D } from 'three'
 
 const DRACO_DECODER_PATH = 'https://www.gstatic.com/draco/versioned/decoders/1.5.6/'
 
-const CAR_OFFSET_X = 22
-const CAR_OFFSET_Y = 1
-const CAR_OFFSET_Z = 14
+const CAR_OFFSET_X = 20
+const CAR_OFFSET_Y = [-0.4, 2.2]
+const CAR_OFFSET_Z = [4.5, 20]
 
-const CAR_ANIM_SPEED = 2.25
+const CAR_ANIM_SPEED = 3
 const CAR_ANIM_DISTANCE = -40
 
 const CyberpunkCar = () => {
@@ -24,10 +24,33 @@ const CyberpunkCar = () => {
 
   const gltf = useLoader(GLTFLoader, '/glb/Cyberpunk-Car.glb', loader => loader.setDRACOLoader(dracoLoader))
 
+  const [direction, setDirection] = useState(1)
+
+  const setRandomYPosition = () => {
+    if (carRef.current) {
+      const randomY = Math.random() * (CAR_OFFSET_Y[1] - CAR_OFFSET_Y[0]) + CAR_OFFSET_Y[0]
+      carRef.current.position.y = randomY
+    }
+  }
+
+  const setRandomZPosition = () => {
+    if (carRef.current) {
+      const randomZ = Math.random() * (CAR_OFFSET_Z[1] - CAR_OFFSET_Z[0]) + CAR_OFFSET_Z[0]
+      carRef.current.position.z = randomZ
+    }
+  }
+
   useEffect(() => {
     if (gltf.scene) {
       carRef.current = gltf.scene.children[0]
-      carRef.current.position.set(CAR_OFFSET_X, CAR_OFFSET_Y, CAR_OFFSET_Z)
+      gltf.scene.children[0].castShadow = true
+      carRef.current.position.set(CAR_OFFSET_X, CAR_OFFSET_Y[0], CAR_OFFSET_Z[0])
+      setRandomYPosition()
+      setRandomZPosition()
+      // Set the initial direction and rotation of the car
+      const initialDirection = Math.random() < 0.5 ? 1 : -1
+      setDirection(initialDirection)
+      carRef.current.rotation.z = initialDirection === 1 ? 0 : Math.PI
       scene.add(gltf.scene)
     }
     return () => {
@@ -41,12 +64,21 @@ const CyberpunkCar = () => {
     if (carRef.current) {
       const elapsedTime = clock.getElapsedTime()
       const loopTime = elapsedTime % CAR_ANIM_SPEED
-      carRef.current.position.x = (loopTime / CAR_ANIM_SPEED) * CAR_ANIM_DISTANCE + CAR_OFFSET_X
-      if (loopTime >= CAR_ANIM_SPEED) {
+      carRef.current.position.x = direction * ((loopTime / CAR_ANIM_SPEED) * CAR_ANIM_DISTANCE + CAR_OFFSET_X)
+      if (Math.abs(loopTime - CAR_ANIM_SPEED) < 0.1) {
         carRef.current.position.x = CAR_OFFSET_X
+        setRandomYPosition()
+        setRandomZPosition()
+        // Change the direction and rotation of the car
+        const newDirection = Math.random() < 0.5 ? 1 : -1
+        setDirection(newDirection)
+        carRef.current.rotation.z = newDirection === 1 ? 0 : Math.PI
+        console.log('direction', newDirection)
+        console.log('carRef.current.rotation.z', carRef.current.rotation.z)
       }
     }
   })
+
   return null
 }
 
