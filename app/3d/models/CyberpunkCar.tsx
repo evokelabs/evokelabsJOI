b0c9a7e42df385ec8afd1f0247d9e30e98404959
Adrian Pikios <adrian@evokelabs.com>
2024-01-08 22:28:33 +1100

    Refactor CyberpunkCar component to add animation and position offset

--- app/3d/models/CyberpunkCar.tsx
+++ app/3d/models/CyberpunkCar.tsx
@@ -1,12 +1,20 @@
-import { useEffect } from 'react'
-import { useLoader, useThree } from '@react-three/fiber'
+import { useEffect, useRef } from 'react'
+import { useLoader, useThree, useFrame } from '@react-three/fiber'
 import { DRACOLoader, GLTFLoader } from 'three/examples/jsm/Addons.js'
-import { Mesh } from 'three'
+import { Object3D } from 'three'
 
 const DRACO_DECODER_PATH = 'https://www.gstatic.com/draco/versioned/decoders/1.5.6/'
 
+const CAR_OFFSET_X = 22
+const CAR_OFFSET_Y = 1
+const CAR_OFFSET_Z = 14
+
+const CAR_ANIM_SPEED = 2.25
+const CAR_ANIM_DISTANCE = -40
+
 const CyberpunkCar = () => {
   const { scene } = useThree()
+  const carRef = useRef<Object3D>()
 
   const dracoLoader = new DRACOLoader()
   dracoLoader.setDecoderPath(DRACO_DECODER_PATH)
@@ -18,6 +26,8 @@ const CyberpunkCar = () => {
 
   useEffect(() => {
     if (gltf.scene) {
+      carRef.current = gltf.scene.children[0]
+      carRef.current.position.set(CAR_OFFSET_X, CAR_OFFSET_Y, CAR_OFFSET_Z)
       scene.add(gltf.scene)
     }
     return () => {
@@ -27,6 +37,16 @@ const CyberpunkCar = () => {
     }
   }, [gltf, scene])
 
+  useFrame(({ clock }) => {
+    if (carRef.current) {
+      const elapsedTime = clock.getElapsedTime()
+      const loopTime = elapsedTime % CAR_ANIM_SPEED
+      carRef.current.position.x = (loopTime / CAR_ANIM_SPEED) * CAR_ANIM_DISTANCE + CAR_OFFSET_X
+      if (loopTime >= CAR_ANIM_SPEED) {
+        carRef.current.position.x = CAR_OFFSET_X
+      }
+    }
+  })
   return null
 }
 
