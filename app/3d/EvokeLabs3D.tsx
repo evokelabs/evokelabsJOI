f4db9df85a16b50b59346d31f972224d408b0cb1
Adrian Pikios <adrian@evokelabs.com>
2024-01-19 01:08:21 +1100

    Add mobile responsiveness to Evokelabs3D component and attach hair model to JOI character

--- app/3d/EvokeLabs3D.tsx
+++ app/3d/EvokeLabs3D.tsx
@@ -11,6 +11,7 @@ import Rain from './particles/Rain'
 import VideoSkybox from './textures/VideoSkyBox'
 import CyberpunkCar from './models/CyberpunkCar/index'
 import JOI from './models/JOI'
+import { useEffect, useState } from 'react'
 
 // const debug = true
 const debug = false
@@ -42,6 +43,26 @@ const CameraRig = () => {
 
 const Evokelabs3D = () => {
   const fov = typeof window !== 'undefined' ? getFov(window.innerWidth) : 50
+  const isMobile = typeof window !== 'undefined' && window.innerWidth <= 768 // Change 768 to the maximum width of a mobile screen in your case
+  const initialVector = isMobile ? new Vector3(0.8, 1.4, 2.5) : new Vector3(0.15, 1.4, 2.5)
+
+  const [vector, setVector] = useState(initialVector)
+
+  useEffect(() => {
+    const handleResize = () => {
+      const isMobile = window.innerWidth <= 768 // Change 768 to the maximum width of a mobile screen in your case
+      const newVector = isMobile ? new Vector3(0.8, 1.4, 2.5) : new Vector3(0.15, 1.4, 2.5)
+      setVector(newVector)
+    }
+
+    // Update the vector when the window size changes
+    window.addEventListener('resize', handleResize)
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   return (
     <Canvas camera={{ position: [0, 1.5, -1], fov: fov, near: 0.1, far: 2000 }} shadows>
@@ -49,7 +70,7 @@ const Evokelabs3D = () => {
       <Environment background files={'./hdr/cyberpunk.hdr'} />
       {debug && <Perf position='top-left' />}
       {!debug && <CameraRig />}
-      <OrbitControls makeDefault target={new Vector3(-0.2, 1.4, 2.5)} enableZoom={debug} enablePan={debug} enableRotate={debug} />
+      <OrbitControls makeDefault target={vector} enableZoom={debug} enablePan={debug} enableRotate={debug} />
       <Lights />
       <CyberpunkMap />
       <JOI />
--- app/3d/models/JOI.tsx
+++ app/3d/models/JOI.tsx
@@ -1,47 +1,56 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useFrame, useThree } from '@react-three/fiber'
-import { AnimationMixer, Box3, Clock, Mesh, Object3D, Vector3 } from 'three'
+import { AnimationMixer, Clock, Object3D } from 'three'
 import { useGLTF, useAnimations } from '@react-three/drei'
+import { SkeletonUtils } from 'three/examples/jsm/Addons.js'
 
 const JOI = () => {
   const { scene, camera } = useThree()
   const mixer = useRef<AnimationMixer | null>(null)
 
   const gltf = useGLTF('/glb/JOI.glb')
+  const gltfHair = useGLTF('/glb/JOI-Hair.glb')
   const { nodes, animations } = gltf
   const model = nodes.Scene || nodes.scene // Replace with the actual name of the root object in your model
   const { actions, names } = useAnimations(animations, model)
 
   const head = nodes.rigHead // Access the head bone
 
+  // const [rotationX, setRotationX] = useState(Math.PI / 4) // Initial rotation
+
   useEffect(() => {
     if (model) {
       scene.add(model)
+
+      // Add the hair to the model
+      const hair = gltfHair.nodes['JOI-Hair'] // Replace with the actual name of the hair object in your model
+
+      // Clone the hair model and attach it to the spine bone
+      const clonedHair = SkeletonUtils.clone(hair)
+      head.add(clonedHair)
+
+      clonedHair.position.set(0.01, 0.175, 0)
+
+      clonedHair.rotation.x = -89.5 // Use the state variable for the rotation
+      clonedHair.rotation.y = Math.PI // Rotate 180 degrees around the y-axis
+      clonedHair.rotation.z = Math.PI // Rotate 90 degrees around the z-axis
 
       if (animations && animations.length > 0) {
         mixer.current = new AnimationMixer(model)
         Object.values(actions).forEach(action => action!.play())
       }
 
-      model.position.x -= 0.1
-
-      const eyeLeftObject = new Object3D()
-      const eyeRightObject = new Object3D()
-
-      model.add(eyeLeftObject)
-      model.add(eyeRightObject)
-
       return () => {
         // Remove the model from the scene
         scene.remove(model)
       }
     }
-  }, [model, scene, actions, names, animations])
+  }, [model, scene, actions, names, animations, gltfHair, head]) // Add rotationX to the dependency array
 
   useFrame(() => {
     mixer.current?.update(new Clock().getDelta())
 
-    // Make the head bone look at the camera
+    // Make the neck bone look at the camera
     if (head) {
       head.lookAt(camera.position)
     }
