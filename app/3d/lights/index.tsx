75c71172715db65f14654e077584b1682832d3fd
Adrian Pikios <adrian@evokelabs.com>
2024-01-19 03:37:39 +1100

    Add animation to ambient light intensity

--- app/3d/lights/index.tsx
+++ app/3d/lights/index.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
+import { gsap } from 'gsap'
 import PointLight from './PointLight'
 import DirectionLight from './DirectionLight'
 
 export const Lights = () => {
+  const ambientLightRef = useRef<THREE.AmbientLight>(null)
+
+  useEffect(() => {
+    if (ambientLightRef.current) {
+      gsap.fromTo(ambientLightRef.current, { intensity: 0 }, { intensity: 1.5, duration: 4, delay: 4 })
+    }
+  }, [])
+
   return (
     <>
       <DirectionLight />
       <PointLight />
       <PointLight positionOffset={[2.88, 0, 0]} />
-      <ambientLight intensity={0.5} color={'#005068'} />
+      <ambientLight ref={ambientLightRef} intensity={1.5} color={'#005068'} />
     </>
   )
 }
