8f0e1a5067eaf1d533a174893752633f3544b5b0
Adrian Pikios <adrian@evokelabs.com>
2024-01-21 01:55:39 +1100

    Add audio playback functionality to Music component

--- app/3d/audio/Music.tsx
+++ app/3d/audio/Music.tsx
@@ -1,38 +1,37 @@
 import { useEffect, useRef } from 'react'
 
+const AUDIO_SOURCE = '/sounds/joi-mix.mp3'
+
 const Music = () => {
   const audioElement = useRef<HTMLAudioElement | null>(null)
 
   useEffect(() => {
+    // Create a new AudioContext and an audio element
     const audioContext = new AudioContext()
     audioElement.current = document.createElement('audio')
-    audioElement.current.src = '/sounds/joi-mix.mp3'
+    audioElement.current.src = AUDIO_SOURCE
     audioElement.current.volume = 0.0
 
+    // Connect the audio element to the AudioContext
     const track = audioContext.createMediaElementSource(audioElement.current)
     track.connect(audioContext.destination)
 
+    // Define a function to play the audio
     const playAudio = () => {
       audioContext.resume().then(() => {
-        if (audioElement.current) {
-          audioElement.current.play()
-        }
+        audioElement.current?.play()
       })
     }
 
+    // Play the audio when it's ready and when the canvas or window is clicked
     audioElement.current.oncanplaythrough = playAudio
-
     const canvas = document.querySelector('canvas')
-    if (canvas) {
-      canvas.addEventListener('click', playAudio)
-    }
-
+    canvas?.addEventListener('click', playAudio)
     window.addEventListener('load', playAudio)
 
+    // Clean up event listeners when the component unmounts
     return () => {
-      if (canvas) {
-        canvas.removeEventListener('click', playAudio)
-      }
+      canvas?.removeEventListener('click', playAudio)
       window.removeEventListener('load', playAudio)
     }
   }, [])
