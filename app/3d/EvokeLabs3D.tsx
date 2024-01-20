d05a8070299417c22ff26e96f46c286f3c3ca213
Adrian Pikios <adrian@evokelabs.com>
2024-01-21 01:54:31 +1100

    create new useCameraSettings custom hook

--- app/3d/EvokeLabs3D.tsx
+++ app/3d/EvokeLabs3D.tsx
@@ -1,8 +1,7 @@
-import { Suspense, useCallback, useEffect, useState } from 'react'
+import { Suspense, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { Vector3 } from 'three'
 
 import CameraRig from './cameras/CameraRig'
 import { Lights } from './lights'
@@ -13,49 +12,24 @@ import Rain from './particles/Rain'
 import JOI from './models/JOI'
 import Music from './audio/Music'
 
-import { getFov } from '../libs/helpers'
 import { AnimationContext } from '../libs/AnimationContext'
+import { useCameraSettings } from '../libs/useCameraSettings'
 
+// Constants
 const debug = false
-// const debug = true
-
-const MOBILE_WIDTH_THRESHOLD = 768
-const INITIAL_TARGET_MOBILE = new Vector3(0.8, 1.4, 2.5)
-const INITIAL_TARGET_DESKTOP = new Vector3(0.15, 1.4, 2.5)
-const DEFAULT_FOV = 50
-
-
-
+const INITIAL_CAMERA_POSITION = [0, 1.5, -1] as const
 
 const Evokelabs3D = () => {
-  const isMobile = typeof window !== 'undefined' && window.innerWidth <= MOBILE_WIDTH_THRESHOLD
-  const initialTarget = isMobile ? INITIAL_TARGET_MOBILE : INITIAL_TARGET_DESKTOP
-  const [cameraTarget, setCameraTarget] = useState(initialTarget)
-
+  // State
   const [shouldAmbientLightPlay, setAmbientLightPlay] = useState(false)
   const [shouldPointLightPlay, setPointLightPlay] = useState(false)
 
-  const initialFov = typeof window !== 'undefined' ? getFov(window.innerWidth) : DEFAULT_FOV
-  const [fov, setFov] = useState(initialFov)
-
-  const handleResize = useCallback(() => {
-    const isMobile = window.innerWidth <= MOBILE_WIDTH_THRESHOLD
-    const newVector = isMobile ? INITIAL_TARGET_MOBILE : INITIAL_TARGET_DESKTOP
-    setCameraTarget(newVector)
-    const newFov = getFov(window.innerWidth)
-    setFov(newFov)
-  }, [])
-
-  useEffect(() => {
-    window.addEventListener('resize', handleResize)
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [handleResize])
+  // Camera settings
+  const { cameraTarget, fov } = useCameraSettings()
 
   return (
     <>
-      <Canvas camera={{ position: [0, 1.5, -1], fov, near: 0.1, far: 2000 }} shadows>
+      <Canvas camera={{ position: INITIAL_CAMERA_POSITION, fov, near: 0.1, far: 2000 }} shadows>
         <VideoSkybox />
         {debug ? <Perf position='top-left' /> : <CameraRig fov={fov} debug={debug} />}
         <OrbitControls makeDefault target={cameraTarget} enableZoom={debug} enablePan={debug} enableRotate={debug} />
---
+++ app/libs/useCameraSettings.tsx
@@ -0,0 +1,41 @@
+import { useCallback, useLayoutEffect, useState } from 'react'
+import { Vector3 } from 'three'
+import { getFov } from '../libs/helpers'
+
+// Constants
+const MOBILE_WIDTH_THRESHOLD = 768
+const INITIAL_TARGET_MOBILE = new Vector3(0.8, 1.4, 2.5)
+const INITIAL_TARGET_DESKTOP = new Vector3(0.15, 1.4, 2.5)
+const DEFAULT_FOV = 50
+
+export const useCameraSettings = () => {
+  // Determine if the device is mobile based on the window width
+  const isMobile = typeof window !== 'undefined' && window.innerWidth <= MOBILE_WIDTH_THRESHOLD
+
+  // Set the initial camera target based on the device type
+  const initialTarget = isMobile ? INITIAL_TARGET_MOBILE : INITIAL_TARGET_DESKTOP
+  const [cameraTarget, setCameraTarget] = useState(initialTarget)
+
+  // Set the initial field of view based on the window width
+  const initialFov = typeof window !== 'undefined' ? getFov(window.innerWidth) : DEFAULT_FOV
+  const [fov, setFov] = useState(initialFov)
+
+  // Handle window resize events
+  const handleResize = useCallback(() => {
+    const isMobile = window.innerWidth <= MOBILE_WIDTH_THRESHOLD
+    const newVector = isMobile ? INITIAL_TARGET_MOBILE : INITIAL_TARGET_DESKTOP
+    setCameraTarget(newVector)
+    const newFov = getFov(window.innerWidth)
+    setFov(newFov)
+  }, [])
+
+  // Add the resize event listener when the component mounts and remove it when it unmounts
+  useLayoutEffect(() => {
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [handleResize])
+
+  return { cameraTarget, fov }
+}
