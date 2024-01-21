59f7cd2ac6f8f0b72add0938a871b0460c7341e9
Adrian Pikios <adrian@evokelabs.com>
2024-01-21 11:07:52 +1100

    Added emission glow effect

--- app/3d/models/JOI.tsx
+++ app/3d/models/JOI.tsx
@@ -1,26 +1,62 @@
 import { useEffect, useRef } from 'react'
 import { useThree } from '@react-three/fiber'
-import { Mesh, Object3D, Scene } from 'three'
+import { Mesh, MeshStandardMaterial, Object3D, Scene } from 'three'
 import { useDracoLoader } from './../../libs/useDracoLoader'
+import { gsap } from 'gsap'
+
+// Constants
+const MODEL_PATH = '/glb/JOI.glb'
+const POSITION = { x: 0.7, y: 0.12, z: 2.1 }
+const ROTATION = { pitch: 0, yaw: Math.PI / 0.9, roll: 0 }
+const EYE_NAMES = ['JOI-Eye-Left', 'JOI-Eye-Right']
+const EMISSIVE_INTENSITY_RANGE = { min: 0.5, max: 1.25 }
+const DURATION_RANGE = { min: 0.25, max: 0.65 }
 
 const JOI = () => {
   const { scene } = useThree()
   const gltfLoader = useRef(useDracoLoader()).current
 
+  // Generate an initial random value for duration
+  let newDuration = gsap.utils.random(DURATION_RANGE.min, DURATION_RANGE.max)
+
+  // Define the onRepeat function
+  const onRepeat = (material: gsap.TweenTarget) => {
+    // Generate a new random value for emissiveIntensity
+    const newEmissiveIntensity = gsap.utils.random(EMISSIVE_INTENSITY_RANGE.min, EMISSIVE_INTENSITY_RANGE.max)
+
+    // Update the random value for duration
+    newDuration = gsap.utils.random(DURATION_RANGE.min, DURATION_RANGE.max)
+
+    // Create a new tween that transitions to the new random value over the new random duration
+    gsap.to(material, {
+      emissiveIntensity: newEmissiveIntensity,
+      duration: newDuration
+    })
+  }
+
   useEffect(() => {
     gltfLoader.load(
-      '/glb/JOI.glb',
+      MODEL_PATH,
       gltf => {
-        gltf.scene.position.set(0.7, 0.12, 2.1) // x, y, z
-        gltf.scene.rotation.set(0, Math.PI / 0.9, 0) // pitch, yaw, roll
+        const { x, y, z } = POSITION
+        gltf.scene.position.set(x, y, z)
 
-        console.log('gltf.scene', gltf.scene)
+        const { pitch, yaw, roll } = ROTATION
+        gltf.scene.rotation.set(pitch, yaw, roll)
 
         const object3DChild = gltf.scene.children.find(child => child instanceof Object3D)
         if (object3DChild) {
           object3DChild.traverse(node => {
-            if (node instanceof Mesh) {
+            if (node instanceof Mesh && EYE_NAMES.includes(node.name)) {
               node.receiveShadow = true
+
+              if (node.material instanceof MeshStandardMaterial) {
+                // Create a GSAP timeline with the onRepeat function as the onRepeat callback
+                const tl = gsap.timeline({ repeat: -1, onRepeat: () => onRepeat(node.material) })
+
+                // Add a dummy tween to the timeline with the initial random duration
+                tl.to({}, { duration: newDuration })
+              }
             }
           })
         }
