05c7128bfe629a31bed0d0327d9fe28f94c5167b
Adrian Pikios <adrian@evokelabs.com>
2024-01-21 12:07:45 +1100

    Refactor eye emission animation and GLTF loading

--- app/3d/models/JOI/controllers/useEyeEmissionAnimation.tsx
+++ app/3d/models/JOI/controllers/useEyeEmissionAnimation.tsx
@@ -1,24 +1,45 @@
 import { useCallback } from 'react'
 import { gsap } from 'gsap'
+import { Mesh, MeshStandardMaterial, Object3D } from 'three'
+import { GLTF } from 'three/examples/jsm/Addons.js'
 
 // Constants for the emission animation
-const EMISSIVE_INTENSITY_RANGE = { min: 0.65, max: 2.25 }
-const DURATION = 0.35
+const EMISSIVE_INTENSITY_RANGE = { min: 0.15, max: 3.75 }
+const DURATION = 0.4
 
 /**
  * Custom hook to create an eye emission animation.
- * Returns a function that starts the animation for a given material.
+ * Returns a function that starts the animation for a given GLTF model.
  */
-export const useEyeEmissionAnimation = (): ((material: gsap.TweenTarget) => void) => {
-  const startEyeEmissionAnimation = useCallback((material: gsap.TweenTarget) => {
-    // Generate a new random value for emissiveIntensity
-    const newEmissiveIntensity = gsap.utils.random(EMISSIVE_INTENSITY_RANGE.min, EMISSIVE_INTENSITY_RANGE.max)
+export const useEyeEmissionAnimation = (): ((gltf: GLTF) => void) => {
+  const startEyeEmissionAnimation = useCallback((gltf: GLTF) => {
+    const object3DChild = gltf.scene.children.find(child => child instanceof Object3D)
+    if (object3DChild) {
+      const materials: MeshStandardMaterial[] = []
 
-    // Create a new tween that transitions to the new random value over the constant duration
-    gsap.to(material, {
-      emissiveIntensity: newEmissiveIntensity,
-      duration: DURATION
-    })
+      object3DChild.traverse(node => {
+        if (node instanceof Mesh && node.material instanceof MeshStandardMaterial) {
+          materials.push(node.material)
+        }
+      })
+
+      // Create a GSAP timeline with the startAnimation function as the onRepeat callback
+      gsap.timeline({
+        repeat: -1,
+        onRepeat: () => {
+          materials.forEach(material => {
+            // Generate a new random value for emissiveIntensity
+            const newEmissiveIntensity = gsap.utils.random(EMISSIVE_INTENSITY_RANGE.min, EMISSIVE_INTENSITY_RANGE.max)
+
+            // Create a new tween that transitions to the new random value over the constant duration
+            gsap.to(material, {
+              emissiveIntensity: newEmissiveIntensity,
+              duration: DURATION
+            })
+          })
+        }
+      })
+    }
   }, [])
 
   return startEyeEmissionAnimation
--- app/3d/models/JOI/index.tsx
+++ app/3d/models/JOI/index.tsx
@@ -1,8 +1,7 @@
-import { useCallback, useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { useThree } from '@react-three/fiber'
-import { Mesh, MeshStandardMaterial, Object3D, Scene } from 'three'
+
 import { useDracoLoader } from './../../../libs/useDracoLoader'
-import { gsap } from 'gsap'
 import { useEyeEmissionAnimation } from './controllers/useEyeEmissionAnimation'
 import { useInitialJOIPositioning } from './controllers/useInitialJOIPositioning'
 
@@ -11,33 +10,17 @@ const MODEL_PATH = '/glb/JOI.glb'
 
 const JOI = () => {
   const { scene } = useThree()
-  const setInitialPositioning = useInitialJOIPositioning()
   const gltfLoader = useRef(useDracoLoader()).current
 
+  const setInitialPositioning = useInitialJOIPositioning()
   const startEyeEmissionAnimation = useEyeEmissionAnimation()
-
-  const traverseObject3DChild = useCallback((gltf: { scene: { children: any[] } }, callback: any) => {
-    const object3DChild = gltf.scene.children.find(child => child instanceof Object3D)
-    if (object3DChild) {
-      object3DChild.traverse(callback)
-    }
-  }, [])
 
   useEffect(() => {
     gltfLoader.load(
       MODEL_PATH,
       gltf => {
         setInitialPositioning(gltf)
-
-        traverseObject3DChild(gltf, (node: { material: gsap.TweenTarget }) => {
-          if (node instanceof Mesh) {
-            if (node.material instanceof MeshStandardMaterial) {
-              // Create a GSAP timeline with the startAnimation function as the onRepeat callback
-              gsap.timeline({ repeat: -1, onRepeat: () => startEyeEmissionAnimation(node.material) })
-            }
-          }
-        })
-
+        startEyeEmissionAnimation(gltf)
         scene.add(gltf.scene)
       },
       undefined,
@@ -45,13 +28,7 @@ const JOI = () => {
         console.error('An error occurred while loading the model:', error)
       }
     )
-
-    return () => {
-      scene.children.forEach(child => {
-        if (child instanceof Scene) scene.remove(child)
-      })
-    }
-  }, [gltfLoader, startEyeEmissionAnimation, scene, setInitialPositioning, traverseObject3DChild])
+  }, [gltfLoader, scene, setInitialPositioning, startEyeEmissionAnimation])
 
   return null
 }
