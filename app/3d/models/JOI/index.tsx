94b67e6e8ee507b699207018056d26b10a67b0a3
Adrian Pikios <adrian@evokelabs.com>
2024-01-23 15:21:32 +1100

    Remove unused code and refactor JOI component

--- app/3d/models/JOI/index.tsx
+++ app/3d/models/JOI/index.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 import { useFrame, useThree } from '@react-three/fiber'
-import { Euler, Matrix4, Mesh, Quaternion, Vector3 } from 'three'
+import { Matrix4, Mesh, Quaternion, Vector3 } from 'three'
 import { useGLTF } from '@react-three/drei'
 import { GLTF } from 'three/examples/jsm/Addons.js'
 
@@ -21,10 +21,6 @@ const JOI = () => {
   useIdleAnimationPoseControl(animations, model, 1.2, true, 1.5)
 
   const head = nodes.mixamorigHead as Mesh // Access the head bone
-  const neck = nodes.mixamorigNeck as Mesh // Access the neck bone
-
-  const axisAngleVector = useRef(new Vector3(-1.5, 0, 0))
-  const piValue = useRef(3.5)
 
   useEffect(() => {
     if (!model) return
@@ -51,7 +47,7 @@ const JOI = () => {
       const v1 = new Vector3()
       const v3 = new Vector3(0, 0, 0)
       const q1 = new Quaternion()
-      const weight =  0 // Change this to control the weight
+      const weight = 0 // Change this to control the weight
       v1.copy(camera.position)
       head.updateWorldMatrix(true, false)
       m1.copy(head.matrixWorld).invert()
