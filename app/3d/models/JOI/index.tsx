a03342110ca11f5450f126a7220d968fd5e845cc
Adrian Pikios <adrian@evokelabs.com>
2024-01-23 15:14:05 +1100

    Refactor JOI model rotation and remove unused code

--- app/3d/models/JOI/index.tsx
+++ app/3d/models/JOI/index.tsx
@@ -1,13 +1,12 @@
 import { useEffect, useRef } from 'react'
 import { useFrame, useThree } from '@react-three/fiber'
-import { Matrix4, Mesh, Quaternion, Vector3 } from 'three'
+import { Euler, Matrix4, Mesh, Quaternion, Vector3 } from 'three'
 import { useGLTF } from '@react-three/drei'
 import { GLTF } from 'three/examples/jsm/Addons.js'
 
 import { useEyeEmissionAnimation } from './controllers/useEyeEmissionAnimation'
 import { useInitialJOIPositioning } from './controllers/useInitialJOIPositioning'
 import { useIdleAnimationPoseControl } from './controllers/useIdleAnimationPoseControl'
-import { GUI } from 'dat.gui'
 
 const JOI = () => {
   const { scene, camera } = useThree()
@@ -28,14 +27,6 @@ const JOI = () => {
   const piValue = useRef(3.5)
 
   useEffect(() => {
-    const gui = new GUI()
-    gui.add(axisAngleVector.current, 'x', -10, 10).step(0.1).name('AxisAngle X')
-    gui.add(axisAngleVector.current, 'y', -10, 10).step(0.1).name('AxisAngle Y')
-    gui.add(axisAngleVector.current, 'z', -10, 10).step(0.1).name('AxisAngle Z')
-    gui.add(piValue, 'current', 0, 10).step(0.1).name('Pi Value')
-  }, [])
-
-  useEffect(() => {
     if (!model) return
 
     scene.add(model)
@@ -54,23 +45,20 @@ const JOI = () => {
     }
   }, [gltf, model, scene, setInitialPositioning, startEyeEmissionAnimation])
 
-  const lowestPoint = 1
-  const highestPoint = 2
-
-  const headTiltDown = 6
-  const headTiltUp = 2.5
-
   useFrame(() => {
     if (head) {
-      const targetRotation = new Quaternion().setFromRotationMatrix(
-        new Matrix4().lookAt(head.position, camera.position, new Vector3(0, 0, 0))
-      )
-
-      piValue.current = headTiltDown + ((camera.position.y - lowestPoint) / (highestPoint - lowestPoint)) * (headTiltUp - headTiltDown)
-
-      const offset = new Quaternion().setFromAxisAngle(axisAngleVector.current, Math.PI / piValue.current)
-      targetRotation.multiply(offset)
-      head.quaternion.slerp(targetRotation, 1)
+      const m1 = new Matrix4()
+      const v1 = new Vector3()
+      const v3 = new Vector3(0, 0, 0)
+      const q1 = new Quaternion()
+      const weight =  0 // Change this to control the weight
+      v1.copy(camera.position)
+      head.updateWorldMatrix(true, false)
+      m1.copy(head.matrixWorld).invert()
+      v1.applyMatrix4(m1)
+      q1.copy(head.quaternion) // Save the original orientation
+      head.quaternion.setFromRotationMatrix(m1.lookAt(v1, v3, head.up))
+      head.quaternion.slerp(q1, weight)
     }
   })
 
