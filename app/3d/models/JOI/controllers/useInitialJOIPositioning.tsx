0ea63d89b87ef886db32f293cfbc728f5d9b05de
Adrian Pikios <adrian@evokelabs.com>
2024-01-24 16:43:24 +1100

    Eye follows camera, new export coords from Blender and adjusted with JOIPoistioning

--- app/3d/models/JOI/controllers/useInitialJOIPositioning.tsx
+++ app/3d/models/JOI/controllers/useInitialJOIPositioning.tsx
@@ -1,11 +1,14 @@
-import { useCallback } from 'react'
+import { useCallback, useRef } from 'react'
 import { Mesh, Object3D } from 'three'
 import { GLTF } from 'three/examples/jsm/Addons.js'
+import { GUI } from 'dat.gui'
 
 // Constants
 const POSITION = { x: 0.7, y: 0.095, z: 2.1 }
-const ROTATION = { pitch: 0, yaw: Math.PI / 0.9, roll: 0 }
+const ROTATION = { pitch: (-90 * Math.PI) / 180, yaw: 0, roll: (-180 * Math.PI) / 180 }
 const EYE_NAMES = ['JOI-Eye-Left', 'JOI-Eye-Right']
+
+// Create a new GUI instance
 
 /**
  * Custom hook to set the initial positioning of the JOI model.
@@ -23,8 +26,6 @@ export const useInitialJOIPositioning = (): ((gltf: GLTF) => void) => {
 
     const { pitch, yaw, roll } = ROTATION
     gltf.scene.rotation.set(pitch, yaw, roll)
-
-    
 
     const object3DChild = gltf.scene.children.find(child => child instanceof Object3D)
     if (object3DChild) {
--- app/3d/models/JOI/index.tsx
+++ app/3d/models/JOI/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
-import { useThree } from '@react-three/fiber'
 import { Mesh } from 'three'
+import { useThree } from '@react-three/fiber'
 import { useGLTF } from '@react-three/drei'
 import { GLTF } from 'three/examples/jsm/Addons.js'
 
@@ -10,7 +10,7 @@ import { useIdleAnimationPoseControl } from './controllers/useIdleAnimationPoseC
 import { useHeadAnimation } from './controllers/useHeadAnimation'
 
 const JOI = () => {
-  const { scene } = useThree()
+  const { scene, camera } = useThree()
 
   const gltf = useGLTF('/glb/JOI.glb')
   const { nodes, animations } = gltf
@@ -27,6 +27,8 @@ const JOI = () => {
 
     scene.add(model)
 
+    console.log('JOI model loaded', model)
+
     setInitialPositioning(gltf as GLTF)
     startEyeEmissionAnimation(gltf as GLTF)
 
@@ -36,10 +38,37 @@ const JOI = () => {
       }
     })
 
+    /// Eye animation
+
+    let rightEye: Mesh<any, any> | null = null
+    let leftEye: Mesh<any, any> | null = null
+
+    model.traverse(object => {
+      if (object instanceof Mesh) {
+        if (object.name === 'JOI-Eye-Right') {
+          rightEye = object
+        } else if (object.name === 'JOI-Eye-Left') {
+          leftEye = object
+        }
+      }
+    })
+
+    /// Update the eyes to face the camera in the animation loop
+    const animate = () => {
+      if (rightEye && leftEye) {
+        // Make the eyes look at the camera
+        rightEye.lookAt(camera.position)
+        leftEye.lookAt(camera.position)
+      }
+      requestAnimationFrame(animate)
+    }
+    animate()
+    ///
+
     return () => {
       scene.remove(model)
     }
-  }, [gltf, model, scene, setInitialPositioning, startEyeEmissionAnimation])
+  }, [camera.position, gltf, model, scene, setInitialPositioning, startEyeEmissionAnimation])
 
   return null
 }
