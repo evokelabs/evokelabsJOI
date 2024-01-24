fccddebe7d73931bf2f27c1ea94345a385f3fe4d
Adrian Pikios <adrian@evokelabs.com>
2024-01-24 23:50:44 +1100

    Randomisation on blinking animation

--- app/3d/models/JOI/controllers/useEyesBlinkingAnimation.tsx
+++ app/3d/models/JOI/controllers/useEyesBlinkingAnimation.tsx
@@ -2,29 +2,49 @@ import { useEffect } from 'react'
 import { Mesh, Object3D } from 'three'
 import { gsap } from 'gsap'
 
+const BLINK_DURATION = 0.1
+const MIN_DELAY = 4
+const MAX_DELAY = 8
+
 export const useEyesBlinkingAnimation = (model: Object3D | undefined) => {
   useEffect(() => {
     if (!model) return
 
+    const tl = gsap.timeline({ repeat: -1, repeatDelay: Math.random() * (MAX_DELAY - MIN_DELAY) + MIN_DELAY })
+    const meshes: Mesh[] = []
+
     model.traverse((object: Object3D) => {
-      if (object instanceof Mesh && object.morphTargetDictionary && object.morphTargetInfluences) {
-        if (object.morphTargetDictionary.Blink !== undefined) {
-          console.log('Blinking')
-          const tl = gsap.timeline({ repeat: -1, repeatDelay: 5 })
-          tl.to(object.morphTargetInfluences, {
-            duration: 0.15,
-            [object.morphTargetDictionary.Blink]: 1
-          })
-            .to(object.morphTargetInfluences, {
-              duration: 0.05,
-              [object.morphTargetDictionary.Blink]: 1
-            })
-            .to(object.morphTargetInfluences, {
-              duration: 0.15,
-              [object.morphTargetDictionary.Blink]: 0
-            })
-        }
+      if (object instanceof Mesh && object.morphTargetDictionary?.Blink !== undefined) {
+        meshes.push(object)
       }
     })
+
+    if (meshes.length > 0 && meshes[0].morphTargetDictionary?.Blink !== undefined) {
+      const blinkIndex = meshes[0].morphTargetDictionary.Blink
+      tl.to(
+        meshes.map(mesh => mesh.morphTargetInfluences),
+        {
+          duration: BLINK_DURATION,
+          [blinkIndex]: 1
+        }
+      )
+        .to(
+          meshes.map(mesh => mesh.morphTargetInfluences),
+          {
+            duration: BLINK_DURATION / 3,
+            [blinkIndex]: 1
+          }
+        )
+        .to(
+          meshes.map(mesh => mesh.morphTargetInfluences),
+          {
+            duration: BLINK_DURATION,
+            [blinkIndex]: 0,
+            onComplete: () => {
+              tl.repeatDelay(Math.random() * (MAX_DELAY - MIN_DELAY) + MIN_DELAY)
+            }
+          }
+        )
+    }
   }, [model])
 }
