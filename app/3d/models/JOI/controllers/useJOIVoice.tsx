b9d5c68407525e353a9830fa3ba64a0d53f80447
Adrian Pikios <adrian@evokelabs.com>
2024-01-25 20:09:01 +1100

    Update JOI voice volume and morph targets

--- app/3d/models/JOI/controllers/useJOIVoice.tsx
+++ app/3d/models/JOI/controllers/useJOIVoice.tsx
@@ -2,7 +2,7 @@ import { AnimationContext } from '@/app/libs/AnimationContext'
 import { useEffect, useState, useContext } from 'react'
 import { SkinnedMesh } from 'three'
 
-const MAX_VOLUME = 100
+const MAX_VOLUME = 160
 const JOI_VOICE_PATH = './sounds/JOI-Voice/'
 const INTRO_01 = 'Intro-01.mp3'
 const INTRO_02 = 'Intro-02.mp3'
@@ -17,13 +17,9 @@ export const useJOIVoice = (model: THREE.Object3D | null) => {
     if (hasPlayed || !shouldJOISpeak || !model) return
 
     const visited = document.cookie.split('; ').find(row => row.startsWith('evokelabs-visited='))
-
-    let audioFile = JOI_VOICE_PATH + INTRO_01
-    if (visited) {
-      const files = [INTRO_02, INTRO_03, INTRO_04]
-      const randomFile = files[Math.floor(Math.random() * files.length)]
-      audioFile = JOI_VOICE_PATH + randomFile
-    }
+    const files = [INTRO_02, INTRO_03, INTRO_04]
+    const randomFile = files[Math.floor(Math.random() * files.length)]
+    const audioFile = `${JOI_VOICE_PATH}${visited ? randomFile : INTRO_01}`
 
     const audio = new Audio(audioFile)
     const audioContext = new AudioContext()
@@ -41,8 +37,12 @@ export const useJOIVoice = (model: THREE.Object3D | null) => {
     const updateMorphTarget = () => {
       const data = new Uint8Array(analyser.frequencyBinCount)
       analyser.getByteFrequencyData(data)
-      const volume = data.reduce((a, b) => a + b) / data.length
-      const morphTargetInfluence = Math.min(Math.max(volume / MAX_VOLUME, 0), 1) // Normalize to 0-1 range, amplify, and clamp to 0-1
+
+      const binSize = Math.floor(data.length / 4)
+      const bins = [data.slice(0, binSize), data.slice(binSize, binSize * 2), data.slice(binSize * 2, binSize * 3), data.slice(binSize * 3)]
+
+      const volumes = bins.map(bin => bin.reduce((a, b) => a + b) / bin.length)
+      const influences = volumes.map(volume => Math.min(Math.max(volume / MAX_VOLUME, 0), 1))
 
       const skinnedMeshes: THREE.SkinnedMesh[] = []
       model.traverse(object => {
@@ -51,13 +51,13 @@ export const useJOIVoice = (model: THREE.Object3D | null) => {
         }
       })
 
-      const jawOpenIndex = skinnedMeshes[0]?.morphTargetDictionary?.JawOpen
+      const { JawOpen: jawOpenIndex, OOO: oooIndex, EEE: eeeIndex } = skinnedMeshes[0]?.morphTargetDictionary || {}
 
-      if (jawOpenIndex !== undefined) {
+      if (jawOpenIndex !== undefined && oooIndex !== undefined && eeeIndex !== undefined) {
         skinnedMeshes.forEach(mesh => {
-          if (mesh.morphTargetInfluences) {
-            mesh.morphTargetInfluences[jawOpenIndex] = morphTargetInfluence
-          }
+          mesh.morphTargetInfluences?.splice(jawOpenIndex, 1, influences[0])
+          mesh.morphTargetInfluences?.splice(oooIndex, 1, influences[1])
+          mesh.morphTargetInfluences?.splice(eeeIndex, 1, influences[2])
         })
       }
 
@@ -66,17 +66,19 @@ export const useJOIVoice = (model: THREE.Object3D | null) => {
       }
     }
 
-    if (!audio.paused) {
-      requestAnimationFrame(updateMorphTarget)
-    }
-
     updateMorphTarget()
 
-    audio.onended = () => {
+    const onAudioEnd = () => {
       if (!visited) {
         document.cookie = 'evokelabs-visited=true'
       }
       setHasPlayed(true)
+    }
+
+    audio.addEventListener('ended', onAudioEnd)
+
+    return () => {
+      audio.removeEventListener('ended', onAudioEnd)
     }
   }, [hasPlayed, shouldJOISpeak, model])
 }
