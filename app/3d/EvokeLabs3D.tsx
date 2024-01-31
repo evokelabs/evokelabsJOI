5972e3ed8098d4bcdd12cfb9eee166a893b90c88
Adrian Pikios <adrian@evokelabs.com>
2024-01-31 21:45:17 +1100

    Add bloom effect GUI to Evokelabs3D component

--- app/3d/EvokeLabs3D.tsx
+++ app/3d/EvokeLabs3D.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
@@ -18,9 +18,11 @@ import { useCameraSettings } from '../libs/useCameraSettings'
 import { EffectComposer, DepthOfField, Bloom, Noise, Vignette, ChromaticAberration, ToneMapping } from '@react-three/postprocessing'
 import { Vector2 } from 'three'
 
+import { GUI } from 'dat.gui'
+
 // Constants
-// const debug = true
-const debug = false
+const debug = true
+// const debug = false
 const INITIAL_CAMERA_POSITION = [0, 1.5, -1] as const
 
 const Evokelabs3D = () => {
@@ -31,6 +33,37 @@ const Evokelabs3D = () => {
 
   // Camera settings
   const { cameraTarget, fov } = useCameraSettings()
+
+  const [gui, setGui] = useState(null)
+
+  const [bloomEffect, setBloomEffect] = useState({
+    radius: 0.7,
+    luminanceThreshold: 0.9,
+    intensity: 0.3,
+    levels: 6,
+    luminanceSmoothing: 0.65
+  })
+
+  useEffect(() => {
+    const gui = new GUI()
+    const bloomFolder = gui.addFolder('Bloom')
+    bloomFolder.add(bloomEffect, 'radius', 0, 1).onChange(value => setBloomEffect({ ...bloomEffect, radius: value }))
+    bloomFolder
+      .add(bloomEffect, 'luminanceThreshold', 0, 1)
+      .onChange(value => setBloomEffect({ ...bloomEffect, luminanceThreshold: value }))
+    bloomFolder.add(bloomEffect, 'intensity', 0, 1).onChange(value => setBloomEffect({ ...bloomEffect, intensity: value }))
+    bloomFolder
+      .add(bloomEffect, 'levels', 0, 20)
+      .step(1)
+      .onChange(value => setBloomEffect({ ...bloomEffect, levels: value }))
+    bloomFolder
+      .add(bloomEffect, 'luminanceSmoothing', 0, 1)
+      .onChange(value => setBloomEffect({ ...bloomEffect, luminanceSmoothing: value }))
+
+    return () => {
+      gui.destroy()
+    }
+  }, [])
 
   return (
     <>
@@ -63,7 +96,14 @@ const Evokelabs3D = () => {
         </AnimationContext.Provider>
         <EffectComposer disableNormalPass>
           <ChromaticAberration offset={new Vector2(0.025, 0.025)} radialModulation={true} modulationOffset={1.1} />
-          <Bloom mipmapBlur radius={0.6} luminanceThreshold={0.75} intensity={0.5} levels={5} luminanceSmoothing={0.025} />
+          <Bloom
+            mipmapBlur
+            radius={bloomEffect.radius}
+            luminanceThreshold={bloomEffect.luminanceThreshold}
+            intensity={bloomEffect.intensity}
+            levels={bloomEffect.levels}
+            luminanceSmoothing={bloomEffect.luminanceSmoothing}
+          />
           <Vignette eskil={false} offset={0.0} darkness={1} />
 
           {/* <DepthOfField focusDistance={0.02} focalLength={0.5} bokehScale={2} height={480} /> */}
