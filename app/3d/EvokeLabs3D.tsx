db9776ba9216bb336d0d0e9d3f25ff3e149c3232
Adrian Pikios <adrian@evokelabs.com>
2024-01-31 20:48:51 +1100

    Add @react-three/postprocessing library and implement ChromaticAberration effect

--- app/3d/EvokeLabs3D.tsx
+++ app/3d/EvokeLabs3D.tsx
@@ -1,7 +1,9 @@
-import { Suspense, useState } from 'react'
+import { Suspense, useEffect, useState, useRef } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import * as dat from 'dat.gui'
+import { BlendFunction } from 'postprocessing'
 
 import CameraRig from './cameras/CameraRig'
 import { Lights } from './lights'
@@ -15,10 +17,11 @@ import Music from './audio/Music'
 import { AnimationContext } from '../libs/AnimationContext'
 import { useCameraSettings } from '../libs/useCameraSettings'
 
-// Constants
-const debug = false
-// const debug = true
+import { EffectComposer, DepthOfField, Bloom, Noise, Vignette, ChromaticAberration, ToneMapping } from '@react-three/postprocessing'
+import { Vector2 } from 'three'
 
+// Constants
+const debug = true
 const INITIAL_CAMERA_POSITION = [0, 1.5, -1] as const
 
 const Evokelabs3D = () => {
@@ -26,13 +29,53 @@ const Evokelabs3D = () => {
   const [shouldAmbientLightPlay, setAmbientLightPlay] = useState(false)
   const [shouldPointLightPlay, setPointLightPlay] = useState(false)
   const [shouldJOISpeak, setShouldJOISpeak] = useState(false)
+  // Effects
+  const [blendFunction, setBlendFunction] = useState(BlendFunction.NORMAL)
+  const [offset, setOffset] = useState(new Vector2(0.001, 0.001))
+  const [radialModulation, setRadialModulation] = useState(true)
+  const [modulationOffset, setModulationOffset] = useState(1)
+  const [opacity, setOpacity] = useState(1)
+
+  //
 
   // Camera settings
   const { cameraTarget, fov } = useCameraSettings()
 
+  // ChromaticAberration instance
+  useEffect(() => {
+    const gui = new dat.GUI()
+    const folder = gui.addFolder('ChromaticAberration')
+
+    const chromaticAberrationSettings = {
+      blendFunction,
+      offset,
+      radialModulation,
+      modulationOffset,
+      opacity
+    }
+
+    folder.add(chromaticAberrationSettings, 'blendFunction', Object.values(BlendFunction)).onChange(value => setBlendFunction(value))
+    folder.add(chromaticAberrationSettings, 'opacity', 0, 1).onChange(value => setOpacity(value))
+    folder.add(chromaticAberrationSettings.offset, 'x', -0.01, 0.01).onChange(value => setOffset(offset.setX(value)))
+    folder.add(chromaticAberrationSettings.offset, 'y', -0.01, 0.01).onChange(value => setOffset(offset.setY(value)))
+    folder.add(chromaticAberrationSettings, 'radialModulation', 0, 1).onChange(value => setRadialModulation(value))
+    folder.add(chromaticAberrationSettings, 'modulationOffset', -0.01, 0.01).onChange(value => setModulationOffset(value))
+
+    return () => gui.destroy()
+  }, [blendFunction, modulationOffset, offset, opacity, radialModulation])
+
   return (
     <>
-      <Canvas camera={{ position: INITIAL_CAMERA_POSITION, fov, near: 0.1, far: 2000 }} shadows>
+      <Canvas
+        camera={{ position: INITIAL_CAMERA_POSITION, fov, near: 0.1, far: 2000 }}
+        shadows
+        gl={{
+          powerPreference: 'high-performance',
+          antialias: false,
+          stencil: false,
+          depth: false
+        }}
+      >
         <VideoSkybox />
         {debug ? <Perf position='top-left' /> : <CameraRig fov={fov} debug={debug} />}
         <OrbitControls makeDefault target={cameraTarget} enableZoom={debug} enablePan={debug} enableRotate={debug} />
@@ -46,15 +89,29 @@ const Evokelabs3D = () => {
             setShouldJOISpeak
           }}
         >
-          <Suspense fallback={null}>
-            <Lights />
-            <CyberpunkMap />
-            <CyberpunkCar />
-          </Suspense>
+          <Lights />
+          <CyberpunkMap />
+          <CyberpunkCar />
           <JOI />
           <Rain />
         </AnimationContext.Provider>
+        <EffectComposer disableNormalPass>
+          <ChromaticAberration
+            blendFunction={blendFunction}
+            offset={offset}
+            radialModulation={radialModulation}
+            modulationOffset={modulationOffset}
+            opacity={opacity}
+          />
+          {/* <DepthOfField focusDistance={0.02} focalLength={0.5} bokehScale={2} height={480} /> */}
+
+          {/* <Bloom luminanceThreshold={0.5} mipmapBlur luminanceSmoothing={0} intensity={1.5} /> */}
+          {/* <Noise opacity={0.1} /> */}
+          {/* <Vignette eskil={false} offset={0.1} darkness={1.1} /> */}
+          {/* <ToneMapping /> */}
+        </EffectComposer>
       </Canvas>
+
       <Music />
     </>
   )
