e54df7cbea64ca9c1fb9397efc9fc2a7a19eab9e
Adrian Pikios <adrian@evokelabs.com>
2024-01-31 20:48:53 +1100

    Missing commit msg

--- app/3d/EvokeLabs3D.tsx
+++ app/3d/EvokeLabs3D.tsx
@@ -1,9 +1,7 @@
-import { Suspense, useEffect, useState, useRef } from 'react'
+import { useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import * as dat from 'dat.gui'
-import { BlendFunction } from 'postprocessing'
 
 import CameraRig from './cameras/CameraRig'
 import { Lights } from './lights'
@@ -21,7 +19,8 @@ import { EffectComposer, DepthOfField, Bloom, Noise, Vignette, ChromaticAberrati
 import { Vector2 } from 'three'
 
 // Constants
-const debug = true
+// const debug = true
+const debug = false
 const INITIAL_CAMERA_POSITION = [0, 1.5, -1] as const
 
 const Evokelabs3D = () => {
@@ -29,40 +28,9 @@ const Evokelabs3D = () => {
   const [shouldAmbientLightPlay, setAmbientLightPlay] = useState(false)
   const [shouldPointLightPlay, setPointLightPlay] = useState(false)
   const [shouldJOISpeak, setShouldJOISpeak] = useState(false)
-  // Effects
-  const [blendFunction, setBlendFunction] = useState(BlendFunction.NORMAL)
-  const [offset, setOffset] = useState(new Vector2(0.001, 0.001))
-  const [radialModulation, setRadialModulation] = useState(true)
-  const [modulationOffset, setModulationOffset] = useState(1)
-  const [opacity, setOpacity] = useState(1)
-
-  //
 
   // Camera settings
   const { cameraTarget, fov } = useCameraSettings()
-
-  // ChromaticAberration instance
-  useEffect(() => {
-    const gui = new dat.GUI()
-    const folder = gui.addFolder('ChromaticAberration')
-
-    const chromaticAberrationSettings = {
-      blendFunction,
-      offset,
-      radialModulation,
-      modulationOffset,
-      opacity
-    }
-
-    folder.add(chromaticAberrationSettings, 'blendFunction', Object.values(BlendFunction)).onChange(value => setBlendFunction(value))
-    folder.add(chromaticAberrationSettings, 'opacity', 0, 1).onChange(value => setOpacity(value))
-    folder.add(chromaticAberrationSettings.offset, 'x', -0.01, 0.01).onChange(value => setOffset(offset.setX(value)))
-    folder.add(chromaticAberrationSettings.offset, 'y', -0.01, 0.01).onChange(value => setOffset(offset.setY(value)))
-    folder.add(chromaticAberrationSettings, 'radialModulation', 0, 1).onChange(value => setRadialModulation(value))
-    folder.add(chromaticAberrationSettings, 'modulationOffset', -0.01, 0.01).onChange(value => setModulationOffset(value))
-
-    return () => gui.destroy()
-  }, [blendFunction, modulationOffset, offset, opacity, radialModulation])
 
   return (
     <>
@@ -96,13 +64,7 @@ const Evokelabs3D = () => {
           <Rain />
         </AnimationContext.Provider>
         <EffectComposer disableNormalPass>
-          <ChromaticAberration
-            blendFunction={blendFunction}
-            offset={offset}
-            radialModulation={radialModulation}
-            modulationOffset={modulationOffset}
-            opacity={opacity}
-          />
+          <ChromaticAberration offset={new Vector2(0.025, 0.025)} radialModulation={true} modulationOffset={1.1} />
           {/* <DepthOfField focusDistance={0.02} focalLength={0.5} bokehScale={2} height={480} /> */}
 
           {/* <Bloom luminanceThreshold={0.5} mipmapBlur luminanceSmoothing={0} intensity={1.5} /> */}
