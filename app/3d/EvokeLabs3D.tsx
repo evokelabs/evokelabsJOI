ce65ee9100b3e64bb8fbe386dffa29f9f455b837
Adrian Pikios <adrian@evokelabs.com>
2024-01-31 22:26:51 +1100

    Update eye emission animation and bloom

--- app/3d/EvokeLabs3D.tsx
+++ app/3d/EvokeLabs3D.tsx
@@ -18,8 +18,6 @@ import { useCameraSettings } from '../libs/useCameraSettings'
 import { EffectComposer, DepthOfField, Bloom, Noise, Vignette, ChromaticAberration, ToneMapping } from '@react-three/postprocessing'
 import { Vector2 } from 'three'
 
-import { GUI } from 'dat.gui'
-
 // Constants
 const debug = true
 // const debug = false
@@ -34,45 +32,13 @@ const Evokelabs3D = () => {
   // Camera settings
   const { cameraTarget, fov } = useCameraSettings()
 
-  const [gui, setGui] = useState(null)
-
-  const [bloomEffect, setBloomEffect] = useState({
-    radius: 0.7,
-    luminanceThreshold: 0.9,
-    intensity: 0.3,
-    levels: 6,
-    luminanceSmoothing: 0.65
-  })
-
-  useEffect(() => {
-    const gui = new GUI()
-    const bloomFolder = gui.addFolder('Bloom')
-    bloomFolder.add(bloomEffect, 'radius', 0, 1).onChange(value => setBloomEffect({ ...bloomEffect, radius: value }))
-    bloomFolder
-      .add(bloomEffect, 'luminanceThreshold', 0, 1)
-      .onChange(value => setBloomEffect({ ...bloomEffect, luminanceThreshold: value }))
-    bloomFolder.add(bloomEffect, 'intensity', 0, 1).onChange(value => setBloomEffect({ ...bloomEffect, intensity: value }))
-    bloomFolder
-      .add(bloomEffect, 'levels', 0, 20)
-      .step(1)
-      .onChange(value => setBloomEffect({ ...bloomEffect, levels: value }))
-    bloomFolder
-      .add(bloomEffect, 'luminanceSmoothing', 0, 1)
-      .onChange(value => setBloomEffect({ ...bloomEffect, luminanceSmoothing: value }))
-
-    return () => {
-      gui.destroy()
-    }
-  }, [])
-
   return (
     <>
       <Canvas
         camera={{ position: INITIAL_CAMERA_POSITION, fov, near: 0.1, far: 2000 }}
         shadows
         gl={{
-          powerPreference: 'high-performance',
-          depth: false
+          powerPreference: 'high-performance'
         }}
       >
         <VideoSkybox />
@@ -96,14 +62,7 @@ const Evokelabs3D = () => {
         </AnimationContext.Provider>
         <EffectComposer disableNormalPass>
           <ChromaticAberration offset={new Vector2(0.025, 0.025)} radialModulation={true} modulationOffset={1.1} />
-          <Bloom
-            mipmapBlur
-            radius={bloomEffect.radius}
-            luminanceThreshold={bloomEffect.luminanceThreshold}
-            intensity={bloomEffect.intensity}
-            levels={bloomEffect.levels}
-            luminanceSmoothing={bloomEffect.luminanceSmoothing}
-          />
+          <Bloom mipmapBlur radius={0.6} luminanceThreshold={0.95} intensity={0.45} luminanceSmoothing={0.65} levels={5} />
           <Vignette eskil={false} offset={0.0} darkness={1} />
 
           {/* <DepthOfField focusDistance={0.02} focalLength={0.5} bokehScale={2} height={480} /> */}
