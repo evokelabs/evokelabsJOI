122ddddad5a1d17dcde3f83cdf0d8f180ebdc60f
Adrian Pikios <adrian@evokelabs.com>
2024-02-12 20:34:57 +1100

    Add dynamic page titles and styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,26 @@ import Evokelabs3D from '@/app/3d/EvokeLabs3D'
 import './../app/globals.css'
 import Head from 'next/head'
 
+import { useRouter } from 'next/router'
+
 export const metadata: Metadata = {
   title: 'EVOKE LABS | Creative Technologist',
   description: 'Welcome to Evoke Labs: the portfolio and business hub of Adrian Pikios, a Sydney-based freelance creative technologist.'
 }
 
 export default function RootLayout() {
+  const router = useRouter()
+
+  const h1Styles = `
+  h1 {
+    color: red;
+    font-size: 2em;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+  }
+`
+
   return (
     <>
       <Head>
@@ -17,6 +31,42 @@ export default function RootLayout() {
         <meta name='description' content={String(metadata.description)} />
       </Head>
       <Evokelabs3D />
+      {router.pathname === '/history' && (
+        <>
+          <h1>history</h1>
+          <style jsx>{h1Styles}</style>
+        </>
+      )}
+      {router.pathname === '/availabilities' && (
+        <>
+          <h1>availabilities</h1>
+          <style jsx>{h1Styles}</style>
+        </>
+      )}
+      {router.pathname === '/joi' && (
+        <>
+          <h1>joi</h1>
+          <style jsx>{h1Styles}</style>
+        </>
+      )}
+      {router.pathname === '/portfolio' && (
+        <>
+          <h1>portfolio</h1>
+          <style jsx>{h1Styles}</style>
+        </>
+      )}
+      {router.pathname === '/resume' && (
+        <>
+          <h1>resume</h1>
+          <style jsx>{h1Styles}</style>
+        </>
+      )}
+      {router.pathname === '/services' && (
+        <>
+          <h1>services</h1>
+          <style jsx>{h1Styles}</style>
+        </>
+      )}
     </>
   )
 }
