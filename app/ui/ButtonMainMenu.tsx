7a9e789713b4f8b36e5b1a1e4ffc438745946304
Adrian Pikios <adrian@evokelabs.com>
2024-02-14 00:56:52 +1100

    Add hover effect to ButtonMainMenu

--- app/ui/ButtonMainMenu.tsx
+++ app/ui/ButtonMainMenu.tsx
@@ -1,21 +1,52 @@
+import { useEffect, useRef } from 'react'
+import { gsap } from 'gsap'
 import LeftFrame from './ButtonMainMenu/LeftFrame'
 import MidFrame from './ButtonMainMenu/MidFrame'
 import IconSmall from './IconSmall'
 
 const ButtonMainMenu = () => {
+  const corpoGuideRef = useRef<HTMLDivElement>(null)
+  const leftFrameRef = useRef<HTMLDivElement>(null)
+  const mainFrameRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const corpoGuide = corpoGuideRef.current
+    const leftFrame = leftFrameRef.current
+    const mainFrame = mainFrameRef.current
+
+    if (corpoGuide && leftFrame && mainFrame) {
+      corpoGuide.addEventListener('mouseenter', () => {
+        gsap.to(corpoGuide, { color: '#53F6FF', duration: 0.225, ease: 'power1.out' })
+        gsap.to(leftFrame, { x: '+5', duration: 0.225, ease: 'power1.out' })
+        gsap.to(mainFrame, { x: '+14', duration: 0.225, ease: 'power1.out' })
+      })
+
+      corpoGuide.addEventListener('mouseleave', () => {
+        gsap.to(corpoGuide, { color: '#F75049', duration: 0.225, ease: 'power1.out' }),
+          gsap.to(leftFrame, { x: '0', duration: 0.225, ease: 'power1.out' })
+        gsap.to(mainFrame, { x: '0', duration: 0.225, ease: 'power1.out' })
+      })
+    }
+  }, [])
+
   return (
-    <div className='relative'>
+    <div className='relative' ref={mainFrameRef}>
       <div className='flex items-center flex-row'>
-        {/* <div className='absolute inset-0 z-0'> */}
-        <LeftFrame />
-        {/* </div> */}
-        {/* <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10'> */}
-        <MidFrame />
-        {/* </div> */}
+        <div ref={leftFrameRef} className='z-1'>
+          <LeftFrame />
+        </div>
+        <div className='z-0'>
+          <MidFrame />
+        </div>
       </div>
-      <div className='absolute flex items-center flex-row top-3.5  font-orbitron place-content-between w-full pl-5 pr-7'>
+      <div
+        className='absolute flex items-center flex-row top-3.5 font-orbitron place-content-between w-full pl-5 pr-7 cursor-pointer'
+        ref={corpoGuideRef}
+      >
         <IconSmall />
-        <div className='top-1.5 relative'>CORPO GUIDE</div>
+        <div className='top-1.5 relative' style={{ textShadow: '4px 0px 0px rgba(222, 84, 86, 0.2), 8px 0px 0px rgba(222, 84, 86, 0.1)' }}>
+          CORPO GUIDE
+        </div>
       </div>
     </div>
   )
