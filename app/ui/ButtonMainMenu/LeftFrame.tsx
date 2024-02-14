53d4715616e62b73125b119b438db85ebc8e0177
Adrian Pikios <adrian@evokelabs.com>
2024-02-14 16:44:32 +1100

    customHook for  animation

--- app/ui/ButtonMainMenu/LeftFrame.tsx
+++ app/ui/ButtonMainMenu/LeftFrame.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef } from 'react'
-import gsap from 'gsap'
+import { useRef } from 'react'
 import { TileFill } from '../libs/TileFill'
 import RedCRTBlur from '../libs/RedCRTBlur'
-import { BLACK, RED, RED_DARK, RED_DULL, UI_DURATION_TIME } from '@/app/libs/UIConstants'
+import { BLACK, RED } from '@/app/libs/UIConstants'
+import useButtonMainMenuController from './useButtonMainMenuController'
 
 const LeftFrame = ({ isHovered, isActive, isMouseDown }: { isHovered: boolean; isActive: boolean; isMouseDown: boolean }) => {
   const svgRef = useRef(null)
@@ -10,45 +10,15 @@ const LeftFrame = ({ isHovered, isActive, isMouseDown }: { isHovered: boolean; i
   const pathBGStrokeRef = useRef(null)
   const pathFGFillRef = useRef(null)
 
-  useEffect(() => {
-    const svg = svgRef.current
-    const pathBGFill = pathBGFillRef.current
-    const pathBGStroke = pathBGStrokeRef.current
-    const pathFGFill = pathFGFillRef.current
-    if (svg) {
-      if (isHovered) {
-        gsap.to(pathBGFill, { attr: { fill: RED_DARK }, duration: UI_DURATION_TIME, ease: 'power1.out' })
-        gsap.to(pathBGStroke, { attr: { 'stroke-opacity': '1' }, duration: UI_DURATION_TIME, ease: 'power1.out' })
-      } else {
-        gsap.to(pathBGFill, { attr: { fill: BLACK }, duration: UI_DURATION_TIME, ease: 'power1.out' })
-        gsap.to(pathBGStroke, { attr: { 'stroke-opacity': '0.6', strokeColor: RED }, duration: UI_DURATION_TIME, ease: 'power1.out' })
-      }
-
-      if (isMouseDown) {
-        gsap.to(pathFGFill, { attr: { 'fill-opacity': '0.5' }, duration: 0, ease: 'power1.out' })
-      } else {
-        gsap.to(pathFGFill, { attr: { 'fill-opacity': '0.1' }, duration: 0, ease: 'power1.out' })
-        gsap.to(pathBGStroke, { attr: { 'stroke-opacity': '1' }, duration: UI_DURATION_TIME, ease: 'power1.out' })
-      }
-
-      if (isActive) {
-        gsap.fromTo(
-          pathFGFill,
-          { attr: { fill: RED, 'fill-opacity': '0.5' } },
-          { attr: { fill: RED_DULL, 'fill-opacity': '0.2' }, duration: UI_DURATION_TIME, ease: 'power1.out' }
-        )
-        gsap.fromTo(
-          pathBGFill,
-          { attr: { fill: RED_DARK, 'fill-opacity': '0.85' } },
-          { attr: { fill: RED_DULL, 'fill-opacity': '1' }, duration: UI_DURATION_TIME, ease: 'power1.out' }
-        )
-      } else if (!isHovered) {
-        gsap.to(pathFGFill, { attr: { 'fill-opacity': '0.1' }, duration: UI_DURATION_TIME, ease: 'power1.out' })
-        gsap.to(pathBGFill, { attr: { fill: BLACK, 'fill-opacity': '0.85' }, duration: UI_DURATION_TIME, ease: 'power1.out' })
-        gsap.to(pathBGStroke, { attr: { 'stroke-opacity': '0.6' }, duration: UI_DURATION_TIME, ease: 'power1.out' })
-      }
-    }
-  }, [isHovered, isActive, isMouseDown])
+  useButtonMainMenuController({
+    isHovered,
+    isActive,
+    isMouseDown,
+    svgRef,
+    pathBGFillRef,
+    pathBGStrokeRef,
+    pathFGFillRef
+  })
 
   return (
     <svg width='16' height='68' viewBox='0 0 16 68' fill='none' ref={svgRef}>
---
+++ app/ui/ButtonMainMenu/useButtonMainMenuController.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useRef } from 'react'
+import { gsap } from 'gsap'
+import { BLACK, RED, RED_DARK, RED_DULL, TEAL, UI_DURATION_TIME } from '@/app/libs/UIConstants'
+
+const useButtonMainMenuController = ({
+  isHovered,
+  isActive,
+  isMouseDown,
+  svgRef,
+  pathBGFillRef,
+  pathBGStrokeRef,
+  pathFGFillRef
+}: {
+  isHovered: boolean
+  isActive: boolean
+  isMouseDown: boolean
+  svgRef: React.MutableRefObject<null>
+  pathBGFillRef: React.MutableRefObject<null>
+  pathBGStrokeRef: React.MutableRefObject<null>
+  pathFGFillRef: React.MutableRefObject<null>
+}) => {
+  const buttonTextRef = useRef<HTMLDivElement>(null)
+  const leftFrameRef = useRef<HTMLDivElement>(null)
+  const mainFrameRef = useRef<HTMLDivElement>(null)
+  const isActiveRef = useRef(isActive)
+
+  useEffect(() => {
+    isActiveRef.current = isActive
+  }, [isActive])
+
+  useEffect(() => {
+    const svg = svgRef.current
+    const pathBGFill = pathBGFillRef.current
+    const pathBGStroke = pathBGStrokeRef.current
+    const pathFGFill = pathFGFillRef.current
+    if (svg) {
+      if (isHovered) {
+        gsap.to(pathBGFill, { attr: { fill: RED_DARK }, duration: UI_DURATION_TIME, ease: 'power1.out' })
+        gsap.to(pathBGStroke, { attr: { 'stroke-opacity': '1' }, duration: UI_DURATION_TIME, ease: 'power1.out' })
+      } else {
+        gsap.to(pathBGFill, { attr: { fill: BLACK }, duration: UI_DURATION_TIME, ease: 'power1.out' })
+        gsap.to(pathBGStroke, { attr: { 'stroke-opacity': '0.6', strokeColor: RED }, duration: UI_DURATION_TIME, ease: 'power1.out' })
+      }
+
+      if (isMouseDown) {
+        gsap.to(pathFGFill, { attr: { 'fill-opacity': '0.5' }, duration: 0, ease: 'power1.out' })
+      } else {
+        gsap.to(pathFGFill, { attr: { 'fill-opacity': '0.1' }, duration: 0, ease: 'power1.out' })
+        gsap.to(pathBGStroke, { attr: { 'stroke-opacity': '1' }, duration: UI_DURATION_TIME, ease: 'power1.out' })
+      }
+
+      if (isActive) {
+        gsap.fromTo(
+          pathFGFill,
+          { attr: { fill: RED, 'fill-opacity': '0.5' } },
+          { attr: { fill: RED_DULL, 'fill-opacity': '0.2' }, duration: UI_DURATION_TIME, ease: 'power1.out' }
+        )
+        gsap.fromTo(
+          pathBGFill,
+          { attr: { fill: RED_DARK, 'fill-opacity': '0.85' } },
+          { attr: { fill: RED_DULL, 'fill-opacity': '1' }, duration: UI_DURATION_TIME, ease: 'power1.out' }
+        )
+      } else if (!isHovered) {
+        gsap.to(pathFGFill, { attr: { 'fill-opacity': '0.1' }, duration: UI_DURATION_TIME, ease: 'power1.out' })
+        gsap.to(pathBGFill, { attr: { fill: BLACK, 'fill-opacity': '0.85' }, duration: UI_DURATION_TIME, ease: 'power1.out' })
+        gsap.to(pathBGStroke, { attr: { 'stroke-opacity': '0.6' }, duration: UI_DURATION_TIME, ease: 'power1.out' })
+      }
+    }
+  }, [isHovered, isActive, isMouseDown, svgRef, pathBGFillRef, pathBGStrokeRef, pathFGFillRef])
+
+  return { buttonTextRef, leftFrameRef, mainFrameRef }
+}
+
+export default useButtonMainMenuController
