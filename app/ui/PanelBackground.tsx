ade80bf43cf23635c4c9c4af4eba64e8fe1497bb
Adrian Pikios <adrian@evokelabs.com>
2024-02-16 05:18:55 +1100

    Add TopHeader component and update PanelBackground layout

--- app/ui/PanelBackground.tsx
+++ app/ui/PanelBackground.tsx
@@ -5,10 +5,25 @@ import ButtonDefault from './ButtonDefault'
 import HR from './HR'
 import TitleHighlight from './TitleHighlight'
 
+const TopHeader = () => {
+  return (
+    <div className='flex items-end'>
+      <div className='absolute'>
+        <h1 className='font-orbitron font-medium text-teal-blur text-[2rem] leading-none px-4 py-4'>BACKSTORY</h1>
+      </div>
+      <div className='inline-block'>
+        <TopHeaderSVG />
+      </div>
+      <div className='bg-red-dark w-full h-[49px] bottom-0 border-red border-2 border-b-0  border-l-0 border-opacity-60'></div>
+    </div>
+  )
+}
+
 const TopHeaderSVG = () => {
   return (
-    <svg width='347' height='58' viewBox='-1 -2 347 58'>
-      <path d='M347 14L333 0H8.5L0 8.5V58L347 57.39' fill={RED_DARK} stroke={RED} strokeWidth='2' strokeOpacity='0.6' />
+    <svg width='349' height='63' viewBox='0 0 349 63'>
+      <path d='M349 16L335 2H10L2 10V64H349' fill={RED_DARK} />
+      <path d='M335.828 0L350.414 14.5858L349 16L335 2H10L2 10V64H349V66H0V9.17157L9.17157 0H335.828Z' fill={RED} fillOpacity={0.6} />
     </svg>
   )
 }
@@ -40,46 +55,46 @@ const BottomRightCornerSVG = () => {
 
 const PanelBackground = () => {
   return (
-    <div className='relative pl-2'>
-      <div className='absolute'>
-        <div className='relative'>
-          <h1 className=' font-orbitron font-medium text-teal-blur text-[2rem] leading-none px-4 py-3'>BACKSTORY</h1>
+    <div className='relative  min-w-[40rem] max-w-[70rem]'>
+      <div className='relative flex flex-row h-full'>
+        <div className='w-auto h-auto grid grid-rows-[63px,auto]'>
+          <div className='h-[63px]'></div>
+          <div className='w-[13px] bg-red-dark border-red border-2 border-r-0 border-opacity-60'></div>
         </div>
-        <div className='bg-red-dark w-full absolute h-[44px] top-3.5 border-red border-2 border-bottom-0 border-b-0 border-opacity-60 -z-10'></div>
 
-        <div className='relative bg-blue-dark my-0.5  px-3 pt-2 h-full'>
-          <div className='flex flex-row items-center gap-1'>
-            <IconSmall />
-            <h2 className='font-rajdhani font-semibold text-red-blur text-[2.25rem] leading-none t'>RESUME</h2>
-            <div className='ml-auto'>
-              <ButtonDefault />
+        <div className='w-full'>
+          <TopHeader />
+          <div className='bg-blue-dark pl-3 pr-6 pt-2 border-red border-t-2 border-x-2 border-opacity-60 pb-3'>
+            <div className='flex flex-row items-center gap-1'>
+              <IconSmall />
+              <h2 className='font-rajdhani font-semibold text-red-blur text-[2.25rem] leading-none t'>RESUME</h2>
+              <div className='ml-auto'>
+                <ButtonDefault />
+              </div>
             </div>
+            <HR />
+            <TitleHighlight title={'BIO'} fullWidth={true} BGColor={RED} />
+            <p className='font-rajdhani text-red-blur text-[1.375rem] font-medium leading-6'>
+              <span className='text-teal-blur font-semibold'>Creative Technologist</span> with over 20+ years of experience in digital
+              strategy, design, UI/UX, web development, motion, and 3D. Proven track record collaborating with advertising agencies,
+              creative strategy, design, UI/UX, web development, motion, and 3D. Proven track record collaborating with advertising
+              agencies, creative years of experience in digital strategy, design, UI/UX, web development, motion, and 3D. Proven track
+              record collaborating with advertising agencies, creative strategy, design, UI/UX, web development, motion, and 3D. Proven
+              track record collaborating with advertising agencies, creative
+            </p>
           </div>
-          <HR />
-          <TitleHighlight title={'BIO'} fullWidth={true} BGColor={RED} />
-          <p className='font-rajdhani text-red-blur text-[1.375rem] font-medium'>
-            <span className='text-teal-blur font-semibold'>Creative Technologist</span> with over 20+ years of experience in digital
-            strategy, design, UI/UX, web development, motion, and 3D. Proven track record collaborating with advertising agencies, creative
-            studios, corporations, and start-ups to deliver hundreds of successful digital projects. Specialising in front-end technologies
-            and fostering positive team relations.
-          </p>
         </div>
-        <div className='absolute -left-[0.95rem]'>
-          <BottomLeftCornerSVG />
-        </div>
-        <div className='absolute right-0'>
-          <BottomRightCornerSVG />
-        </div>
+        {/* <div className='absolute -left-[0.95rem]'>
+            <BottomLeftCornerSVG />
+          </div> */}
+        {/* <div className='absolute right-0'>
+            <BottomRightCornerSVG />
+          </div> */}
       </div>
 
-      <div>
-        <TopHeaderSVG />
-      </div>
-
-      <div className='absolute top-4 -left-1.5'>
+      {/* <div className='absolute top-4 -left-1.5'>
         <TopLeftCornerSVG />
-      </div>
-      <div className='absolute left-0 w-[12px] h-full bg-red-dark  border-red border-2 border-opacity-60 border-y-0 '></div>
+      </div> */}
     </div>
   )
 }
