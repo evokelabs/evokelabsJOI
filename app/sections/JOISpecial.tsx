0bf06293f53b202957e84fb053fddd8f2fa618ea
Adrian Pikios <adrian@evokelabs.com>
2024-02-20 20:27:15 +1100

    Update JOISpecial component styling

--- app/sections/JOISpecial.tsx
+++ app/sections/JOISpecial.tsx
@@ -22,15 +22,15 @@ const JOISpecial = () => {
         <video className='w-full h-full' controls src='./videos/JOI-Introduction.mp4' />
       </div>
 
-      <div className='h-2 border-b-2 bg-grid-darkRed border-l-2 border-red border-opacity-60 mr-2 relative '>
-        <div className='absolute -right-3.5'>
-          <svg width='16' height='8' viewBox='0 0 16 8'>
+      <div className='h-2 border-b-2 bg-grid-darkRed border-l-2 border-red border-opacity-60 mr-2 relative pb-2 '>
+        <div className='absolute -right-3'>
+          <svg width='16' height='10' viewBox='0 0 16 10'>
             <RedCRTBlur />
             <g filter='url(#RedCRTBlur1) url(#RedCRTBlur2)'>
-              <path d='M0 0H10V1L3 8H0V0Z' fill={BLACK} fillOpacity='0.85' />
-              <path d='M0 0H10V1L3 8H0V0Z' fill={RED_BLACK} fillOpacity='0.1' />
-              <path d='M0 0H10V1L3 8H0V0Z' fill='url(#redTile)' />
-              <path d='M10 1L3 8H0V6H2L7.92339 0H10V1Z' fill={RED} fillOpacity={0.6} />
+              <path d='M0 0H12V1L3 10H0V0Z' fill={BLACK} />
+              <path d='M0 0H12V1L3 10H0V0Z' fill={RED_BLACK} fillOpacity='0.1' />
+              <path d='M0 0H12V1L3 10H0V0Z' fill='url(#redTile)' fillOpacity='0.85' />
+              <path d='M12 1L3 10H0V8H2L9.92339 0H12V1Z' fill={RED} fillOpacity={0.6} />
             </g>
           </svg>
         </div>
