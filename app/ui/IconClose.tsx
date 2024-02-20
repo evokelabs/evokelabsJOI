fae1ad49c56db882db6f7d31e3224313f9b20be5
Adrian Pikios <adrian@evokelabs.com>
2024-02-20 20:35:55 +1100

    Refactor IconClose component to adjust positioning and add stroke

--- app/ui/IconClose.tsx
+++ app/ui/IconClose.tsx
@@ -31,14 +31,13 @@ const IconClose = () => {
     pathFGFillRef
   })
   return (
-    <div className='cursor-pointer relative left-1.5' style={{ pointerEvents: isActive ? 'none' : 'all' }}>
-      <svg width='44' height='34' viewBox='-1 0 44 34' ref={svgRef}>
+    <div className='cursor-pointer relative -left-0.5' style={{ pointerEvents: isActive ? 'none' : 'all' }}>
+      <svg width='34' height='34' viewBox='0 0 34 34' ref={svgRef}>
         <rect ref={pathBGFillRef} x='0' y='0' width='34' height='34' fill={BLUE_DARK} fillOpacity='0.85' />
         <rect ref={pathFGFillRef} x='0' y='0' width='34' height='34' fill={RED} fillOpacity='0.1' />
-        <rect x='0' y='0' width='34' height='34' fill='url(#redTile)' />
+        <rect x='0' y='0' width='34' height='34' fill='url(#redTile)' stroke={RED} strokeWidth='2' strokeOpacity={'0.6'} />
         <RedCRTBlur />
         <g filter='url(#RedCRTBlur1) url(#RedCRTBlur2)'>
-          <rect x='0' y='1' width='34' height='32' stroke={RED} strokeWidth='2' strokeOpacity={'0.6'} fillOpacity='0' />
           <path
             ref={pathBGStrokeRef}
             d='M23.338 8H25.9746L18.3183 17L26 26H23.338L17 18.575L10.6366 26H8L15.6563 17L8 8H10.6366L17 15.425L23.338 8Z'
