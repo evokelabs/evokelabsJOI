6437a2cac0603eb62dbb9df550d634d4aa965484
Adrian Pikios <adrian@evokelabs.com>
2024-02-20 20:53:24 +1100

    Refactor JOISpecial component to use VideoFrame

--- app/sections/JOISpecial.tsx
+++ app/sections/JOISpecial.tsx
@@ -1,7 +1,6 @@
-import { BLACK, RED, RED_BLACK } from '../libs/UIConstants'
 import IconSmall from '../ui/IconSmall'
 import PanelBackground from '../ui/PanelBackground'
-import RedCRTBlur from '../ui/libs/RedCRTBlur'
+import VideoFrame from '../ui/VideoFrame'
 
 const JOISpecial = () => {
   return (
@@ -18,23 +17,7 @@ const JOISpecial = () => {
           </p>
         </div>
       </div>
-      <div className='w-full bg-grid-darkRed h-full border-red border-t-2 border-x-2 border-opacity-60 mt-3 p-2 pb-0 border-b-0 shadow-red-blur'>
-        <video className='w-full h-full' controls src='./videos/JOI-Introduction.mp4' />
-      </div>
-
-      <div className='h-2 border-b-2 bg-grid-darkRed border-l-2 border-red border-opacity-60 mr-2 relative pb-2 '>
-        <div className='absolute -right-3'>
-          <svg width='16' height='10' viewBox='0 0 16 10'>
-            <RedCRTBlur />
-            <g filter='url(#RedCRTBlur1) url(#RedCRTBlur2)'>
-              <path d='M0 0H12V1L3 10H0V0Z' fill={BLACK} />
-              <path d='M0 0H12V1L3 10H0V0Z' fill={RED_BLACK} fillOpacity='0.1' />
-              <path d='M0 0H12V1L3 10H0V0Z' fill='url(#redTile)' fillOpacity='0.85' />
-              <path d='M12 1L3 10H0V8H2L9.92339 0H12V1Z' fill={RED} fillOpacity={0.6} />
-            </g>
-          </svg>
-        </div>
-      </div>
+      <VideoFrame videoURL='./videos/JOI-Introduction.mp4' />
     </PanelBackground>
   )
 }
