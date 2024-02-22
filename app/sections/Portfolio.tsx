e0f571d857d307112a7c92c28ccf05340428620a
Adrian Pikios <adrian@evokelabs.com>
2024-02-22 20:52:42 +1100

    Update Portfolio dropdown menu styling

--- app/sections/Portfolio.tsx
+++ app/sections/Portfolio.tsx
@@ -45,14 +45,14 @@ const DropdownMenu: React.FC<DropdownMenuProps> = ({ options, defaultOption, onS
         <button
           ref={buttonRef}
           type='button'
-          className='inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500 uppercase'
+          className='flex items-center  border-2-red px-3 pt-0.5 pb-1 font-medium uppercase text-teal-blur text-[16px] bg-grid-red shadow-red-blur'
           id='options-menu'
           aria-haspopup='true'
           aria-expanded='true'
           onClick={() => setIsOpen(!isOpen)}
         >
           {selectedOption}
-          <svg className='-mr-1 ml-2 h-5 w-5' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' aria-hidden='true'>
+          <svg className='-mr-1 ml-2 h-5 w-5' viewBox='0 0 20 20' fill='currentColor' aria-hidden='true'>
             <path
               fillRule='evenodd'
               d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z'
