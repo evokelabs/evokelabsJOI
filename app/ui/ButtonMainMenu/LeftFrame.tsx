451ce3d82d23356b80268bc84a217b14846279de
Adrian Pikios <adrian@evokelabs.com>
2024-02-24 04:19:06 +1100

    Refactor LeftFrame component

--- app/ui/ButtonMainMenu/LeftFrame.tsx
+++ app/ui/ButtonMainMenu/LeftFrame.tsx
@@ -40,28 +40,29 @@ const LeftFrame = ({
     <svg width='16' height='68' viewBox='0 0 16 68' fill='none' ref={svgRef}>
       <path
         ref={pathBGFillRef}
-        d='M14.0019 2L14.002 66L9 66L2.00195 59L2.00195 49L7 44L7 25L2.00195 20L2.00195 8.99999L9 2L14.0019 2Z'
+        d='M14 2L14 66L8.99805 66L2 59L2 49L6.99805 44L6.99805 25L2 20L2 8.99999L8.99805 2L14 2Z'
         fill={secondaryColor}
         fillOpacity='0.85'
       />
 
       <path
         ref={pathFGFillRef}
-        d='M14.0019 2L14.002 66L9 66L2.00195 59L2.00195 49L7 44L7 25L2.00195 20L2.00195 8.99999L9 2L14.0019 2Z'
+        d='M14 2L14 66L8.99805 66L2 59L2 49L6.99805 44L6.99805 25L2 20L2 8.99999L8.99805 2L14 2Z'
         fill={primaryColor}
         fillOpacity='0.1'
       />
 
-      <path d='M14.0019 2L14.002 66L9 66L2.00195 59L2.00195 49L7 44L7 25L2.00195 20L2.00195 8.99999L9 2L14.0019 2Z' fill='url(#redTile)' />
+      <path d='M14 2L14 66L8.99805 66L2 59L2 49L6.99805 44L6.99805 25L2 20L2 8.99999L8.99805 2L14 2Z' fill='url(#redTile)' />
       <RedCRTBlur />
       <OrangeCRTBlur />
       <g filter={filterBlur}>
         <path
           ref={pathBGStrokeRef}
-          d='M14.002 67L15.002 67L15.002 66L15.0019 2L15.0019 1L14.0019 1L9 1L8.5857 1L8.29279 1.29299L1.29475 8.29299L1.00195 8.58586L1.00195 8.99999L1.00195 20L1.00195 20.4141L1.29471 20.707L6 25.4141L6 43.5859L1.29471 48.293L1.00195 48.5859L1.00195 49L1.00195 59L1.00195 59.4141L1.29475 59.707L8.29279 66.707L8.58571 67L9 67L14.002 67Z'
-          stroke={primaryColor}
-          strokeOpacity='0.6'
-          strokeWidth='2'
+          d='M16 68L8.16946 68L-6.91895e-07 59.8283L2.28507e-07 48.1718L4.99805 43.1718L4.99805 25.8282L-1.79309e-07 20.8282L-2.97249e-07 8.17173L8.16946 7.06829e-07L16 0L16 68ZM6.99805 25L6.99805 44L2 49L2 59L8.99805 66L14 66L14 2L8.99805 2L2 8.99999L2 20L6.99805 25Z'
+          fill={primaryColor}
+          fillOpacity='0.6'
+          fillRule='evenodd'
+          clipRule='evenodd'
         />
       </g>
       {RED_TILE_PATTERN}
