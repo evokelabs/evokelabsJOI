a62e6d4cb7f91b8172240cd34bb77e32dd0d7a7e
Adrian Pikios <adrian@evokelabs.com>
2024-02-24 04:36:42 +1100

    Refactor button icon animation

--- app/ui/libs/useButtonIconController.tsx
+++ app/ui/libs/useButtonIconController.tsx
@@ -35,10 +35,10 @@ const useButtonIconController = ({
       if (isHovered) {
         gsap.to(pathBGStroke, { attr: { 'fill-opacity': 1 }, duration: UI_DURATION_TIME, ease: 'power1.out' })
         gsap.to(pathBGFill, { attr: { fill: BLACK, 'fill-opacity': 0.5 }, duration: UI_DURATION_TIME, ease: 'power1.out' })
-        gsap.to(pathBGStroke, { attr: { fill: TEAL }, duration: UI_DURATION_TIME, ease: 'power1.out' })
+        // gsap.to(pathBGStroke, { attr: { fill: TEAL }, duration: UI_DURATION_TIME, ease: 'power1.out' })
       } else {
         gsap.to(pathBGStroke, { attr: { 'fill-opacity': 0.6, strokeColor: RED }, duration: UI_DURATION_TIME, ease: 'power1.out' })
-        gsap.to(pathBGStroke, { attr: { fill: RED }, duration: UI_DURATION_TIME, ease: 'power1.out' })
+        // gsap.to(pathBGStroke, { attr: { fill: RED }, duration: UI_DURATION_TIME, ease: 'power1.out' })
       }
 
       if (isActive) {
@@ -47,7 +47,7 @@ const useButtonIconController = ({
           { attr: { fill: BLACK, 'fill-opacity': 0.5 } },
           { attr: { fill: BLACK, 'fill-opacity': 0.4 }, duration: 0.5, ease: 'power1.out' }
         )
-        gsap.to(pathBGStroke, { attr: { fill: TEAL, 'fill-opacity': 1 }, duration: 0.5, ease: 'power1.out' })
+        gsap.to(pathBGStroke, { attr: { 'fill-opacity': 1 }, duration: 0.5, ease: 'power1.out' })
       }
     }
   }, [isActive, isHovered, pathBGFillRef, pathBGStrokeRef, svgRef])
