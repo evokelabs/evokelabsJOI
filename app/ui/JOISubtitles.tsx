a46f01e5329850d80162776568a112175efba5f9
Adrian Pikios <adrian@evokelabs.com>
2024-03-05 09:01:55 +1100

    Refactor JOISubtitles component to dynamically change opacity based on audio playback

--- app/ui/JOISubtitles.tsx
+++ app/ui/JOISubtitles.tsx
@@ -3,14 +3,13 @@ import { JOISpeechContext } from '../libs/JOISpeechContext'
 
 const JOISubtitles = () => {
   const { JOILineCaption, isAudioPlaying } = useContext(JOISpeechContext)
-
-  if (!isAudioPlaying) {
-    return null
-  }
+  const opacityClass = isAudioPlaying ? 'opacity-100' : 'opacity-0'
 
   return (
-    <div className='absolute bottom-4 pt-5 flex flex-row justify-center items-center z-[10000000000000000] w-full font-semibold pointer-events-none'>
-      <p className='text-red-blur text-4xl bg-black px-4 py-2 bg-opacity-50 shadow-lg'>
+    <div
+      className={`absolute bottom-4 pt-5 flex flex-row justify-center items-center z-[10000000000000000] w-full font-semibold pointer-events-none transition-opacity duration-500 ${opacityClass}`}
+    >
+      <p className='text-red-blur text-4xl bg-black px-4 py-2 bg-opacity-40 shadow-lg'>
         <span className='text-teal-blur'>JOI:</span> {JOILineCaption}
       </p>
     </div>
