f5202d672c818256ecdcb415e5e547aa44facbed
Adrian Pikios <adrian@evokelabs.com>
2024-03-05 09:04:16 +1100

    Add useEffect hook to handle audio playback timeout

--- app/ui/JOISubtitles.tsx
+++ app/ui/JOISubtitles.tsx
@@ -1,8 +1,23 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { JOISpeechContext } from '../libs/JOISpeechContext'
 
 const JOISubtitles = () => {
-  const { JOILineCaption, isAudioPlaying } = useContext(JOISpeechContext)
+  const { JOILineCaption, isAudioPlaying, setIsAudioPlaying } = useContext(JOISpeechContext)
+
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout
+    if (isAudioPlaying) {
+      timeoutId = setTimeout(() => {
+        setIsAudioPlaying(false)
+      }, 10000)
+    }
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId)
+      }
+    }
+  }, [isAudioPlaying, setIsAudioPlaying])
+
   const opacityClass = isAudioPlaying ? 'opacity-100' : 'opacity-0'
 
   return (
