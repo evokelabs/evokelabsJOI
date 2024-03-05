1b671457352723e447f68b53ff2e66fec3475768
Adrian Pikios <adrian@evokelabs.com>
2024-03-05 12:50:49 +1100

    Update SoundControlIcons component

--- app/ui/SoundControlIcons.tsx
+++ app/ui/SoundControlIcons.tsx
@@ -6,9 +6,10 @@ import SoundJOIIconSVG from './svg/button/SoundJOIIconSVG'
 import SoundControlIconOnSVG from './svg/button/SoundControlIconOnSVG'
 import SoundControlIconOffSVG from './svg/button/SoundControlIconOffSVG'
 import ButtonAlternative from './ButtonAlternative'
+import SoundCrossIconSVG from './svg/button/SoundCrossIconSVG'
 
 const SoundControlIcons = () => {
-  const [soundControlMasterToggle, setsoundControlMasterToggle] = useState(false)
+  const [soundControlMasterToggle, setSoundControlMasterToggle] = useState(false)
   const [musicToggle, setMusicToggle] = useState(false)
   const [SFXToggle, setSFXToggle] = useState(false)
   const [rainToggle, setRainToggle] = useState(false)
@@ -36,22 +37,52 @@ const SoundControlIcons = () => {
 
   return (
     <div className='absolute bottom-5 right-0 pt-5 flex flex-row z-[10000000000000000]'>
-      <div onClick={() => setMusicToggle(!musicToggle)}>
+      <div onClick={() => setMusicToggle(!musicToggle)} className='relative'>
+        <div className='absolute mt-1.5 right-0 mr-2'>
+          <SoundCrossIconSVG />
+        </div>
         <ButtonAlternative SVGIcon={props => <SoundMusicIconSVG {...props} />} />
       </div>
-      <div onClick={() => setSFXToggle(!SFXToggle)}>
+      <div onClick={() => setSFXToggle(!SFXToggle)} className='relative'>
+        <div className='absolute mt-1.5 right-0 mr-2'>
+          <SoundCrossIconSVG />
+        </div>
         <ButtonAlternative SVGIcon={props => <SoundSFXIconSVG {...props} />} />
       </div>
-      <div onClick={() => setRainToggle(!rainToggle)}>
+      <div onClick={() => setRainToggle(!rainToggle)} className='relative'>
+        <div className='absolute mt-1.5 right-0 mr-2'>
+          <SoundCrossIconSVG />
+        </div>
         <ButtonAlternative SVGIcon={props => <SoundRainIconSVG {...props} />} />
       </div>
-      <div onClick={() => setJOIToggle(!JOIToggle)}>
+      <div onClick={() => setJOIToggle(!JOIToggle)} className='relative'>
+        <div className='absolute mt-1.5 right-0 mr-2'>
+          <SoundCrossIconSVG />
+        </div>
         <ButtonAlternative SVGIcon={props => <SoundJOIIconSVG {...props} />} />
       </div>
-      <div onClick={() => setsoundControlMasterToggle(!soundControlMasterToggle)}>
+      <div onClick={() => setSoundControlMasterToggle(!soundControlMasterToggle)} className='relative'>
         <ButtonAlternative SVGIcon={props => <SoundControlIconOffSVG {...props} />} />
       </div>
-      <div onClick={() => setsoundControlMasterToggle(!soundControlMasterToggle)}>
+      <div onClick={() => setSoundControlMasterToggle(!soundControlMasterToggle)} className='relative'>
+        <ButtonAlternative SVGIcon={props => <SoundControlIconOnSVG {...props} />} />
+      </div>
+      <div onClick={() => setMusicToggle(!musicToggle)} className='relative'>
+        <ButtonAlternative SVGIcon={props => <SoundMusicIconSVG {...props} />} />
+      </div>
+      <div onClick={() => setSFXToggle(!SFXToggle)} className='relative'>
+        <ButtonAlternative SVGIcon={props => <SoundSFXIconSVG {...props} />} />
+      </div>
+      <div onClick={() => setRainToggle(!rainToggle)} className='relative'>
+        <ButtonAlternative SVGIcon={props => <SoundRainIconSVG {...props} />} />
+      </div>
+      <div onClick={() => setJOIToggle(!JOIToggle)} className='relative'>
+        <ButtonAlternative SVGIcon={props => <SoundJOIIconSVG {...props} />} />
+      </div>
+      <div onClick={() => setSoundControlMasterToggle(!soundControlMasterToggle)} className='relative'>
+        <ButtonAlternative SVGIcon={props => <SoundControlIconOffSVG {...props} />} />
+      </div>
+      <div onClick={() => setSoundControlMasterToggle(!soundControlMasterToggle)} className='relative'>
         <ButtonAlternative SVGIcon={props => <SoundControlIconOnSVG {...props} />} />
       </div>
     </div>
--- app/ui/svg/button/SoundCrossIconSVG.tsx
+++ app/ui/svg/button/SoundCrossIconSVG.tsx
@@ -2,16 +2,16 @@ import { RED, TEAL } from '@/app/libs/UIConstants'
 import React from 'react'
 import RedCRTBlur from '../../libs/RedCRTBlur'
 
-const SoundCrossIconSVG = ({ isHovered }: { isHovered: boolean }) => {
+const SoundCrossIconSVG = () => {
   return (
-    <svg width='46' height='46' viewBox='0 0 46 46' fill='none'>
+    <svg width='30' height='10' viewBox='0 0 10 10' fill='none'>
       <RedCRTBlur />
       <g filter='url(#RedCRTBlur1) url(#RedCRTBlur2)'>
         <path
           fillRule='evenodd'
           clipRule='evenodd'
-          d='M33 31H29.0961V24.3508C29.0961 22.5277 28.4034 21.509 26.9604 21.509C25.3907 21.509 24.5706 22.5692 24.5706 24.3508V31H20.8083V18.3333H24.5706V20.0395C24.5706 20.0395 25.7018 17.9463 28.3898 17.9463C31.0765 17.9463 33 19.587 33 22.9803V31ZM16.32 16.6747C15.0384 16.6747 14 15.6281 14 14.3374C14 13.0466 15.0384 12 16.32 12C17.6015 12 18.6393 13.0466 18.6393 14.3374C18.6393 15.6281 17.6015 16.6747 16.32 16.6747ZM14.3773 31H18.3004V18.3333H14.3773V31Z'
-          fill={isHovered ? TEAL : RED}
+          d='M8.40158 0.293385L9.70743 1.59659L6.30437 5.00016L9.70743 8.40373L8.40158 9.70694L4.99929 6.30414L1.59658 9.70735L0.292969 8.40331L3.69561 5.00016L0.292969 1.59701L1.59658 0.292969L4.99929 3.69618L8.40158 0.293385Z'
+          fill={RED}
         />
       </g>
     </svg>
