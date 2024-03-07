e095862809eed1d6b3b1938bd98afec31f205066
Adrian Pikios <adrian@evokelabs.com>
2024-03-08 05:19:42 +1100

    Removed console.logs

--- app/ui/JOISubtitles.tsx
+++ app/ui/JOISubtitles.tsx
@@ -1,17 +1,50 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import { JOISpeechContext } from '../libs/JOISpeechContext'
+
+const CAPTION_WAIT_TIMER_COOKIE = 4500
+const MENU_HOME_WAIT_TIMER_NOCOOKIE = 7500
 
 const JOISubtitles = () => {
   const { JOILineCaption, isAudioPlaying, setIsAudioPlaying, isChainPlaying, setIsChainPlaying } = useContext(JOISpeechContext)
 
   const [currentCaption, setCurrentCaption] = useState(JOILineCaption)
+  const [menuHomeWaitTimerNoCookie, setMenuHomeWaitTimerNoCookie] = useState(MENU_HOME_WAIT_TIMER_NOCOOKIE)
 
-  const CAPTION_WAIT_TIMER_COOKIE = 4500
-  const MENU_HOME_WAIT_TIMER_NOCOOKIE = 7500
+  const initialTimerRef = useRef<number | null>(null)
 
-  const visitedCookie =
-    typeof document !== 'undefined' ? document.cookie.split('; ').find(row => row.startsWith('evokelabs-visited=')) : null
-  const initialTimer = visitedCookie ? CAPTION_WAIT_TIMER_COOKIE : MENU_HOME_WAIT_TIMER_NOCOOKIE
+  useEffect(() => {
+    if (initialTimerRef.current === null) {
+      const visitedCookie =
+        typeof document !== 'undefined' ? document.cookie.split('; ').find(row => row.startsWith('evokelabs-visited=')) : null
+      initialTimerRef.current = visitedCookie ? CAPTION_WAIT_TIMER_COOKIE : MENU_HOME_WAIT_TIMER_NOCOOKIE
+      setMenuHomeWaitTimerNoCookie(initialTimerRef.current)
+
+      // Set a timeout to update initialTimerRef.current to 4500 after 18 seconds
+      const timer = setTimeout(() => {
+        initialTimerRef.current = 4500
+      }, 18000)
+
+      // Clear the timeout when the component unmounts
+      return () => clearTimeout(timer)
+    }
+  }, [])
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMenuHomeWaitTimerNoCookie(4500)
+    }, 18000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMenuHomeWaitTimerNoCookie(4500)
+    }, menuHomeWaitTimerNoCookie)
+
+    return () => clearTimeout(timer)
+  }, [menuHomeWaitTimerNoCookie])
+
   useEffect(() => {
     if (!isAudioPlaying && !isChainPlaying) {
       setTimeout(() => {
@@ -24,27 +57,13 @@ const JOISubtitles = () => {
   }, [JOILineCaption, isAudioPlaying, isChainPlaying, setIsAudioPlaying, setIsChainPlaying])
 
   useEffect(() => {
-    let timeoutId: NodeJS.Timeout
-    let innerTimeoutId: NodeJS.Timeout
-
-    const SAFEGUARD_TIMER = isAudioPlaying ? initialTimer : 150
-    timeoutId = setTimeout(() => {
+    const SAFEGUARD_TIMER = isAudioPlaying ? initialTimerRef.current ?? 0 : 150
+    const timeoutId = setTimeout(() => {
       setIsAudioPlaying(false)
       setIsChainPlaying(false)
-
-      innerTimeoutId = setTimeout(() => {
-        // setCurrentCaption(null)
-      }, 550)
     }, SAFEGUARD_TIMER)
 
-    return () => {
-      if (timeoutId) {
-        clearTimeout(timeoutId)
-      }
-      if (innerTimeoutId) {
-        clearTimeout(innerTimeoutId)
-      }
-    }
+    return () => clearTimeout(timeoutId)
   }, [isAudioPlaying, setIsAudioPlaying, setIsChainPlaying])
 
   const opacityClass = isAudioPlaying ? 'opacity-100' : 'opacity-0'
