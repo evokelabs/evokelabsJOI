40180a436ed2c7a8d6d75cf57b27af04b413af03
Adrian Pikios <adrian@evokelabs.com>
2024-03-10 04:52:50 +1100

    red box bg

--- app/ui/VideoFrame.tsx
+++ app/ui/VideoFrame.tsx
@@ -116,9 +116,9 @@ const VideoFrame = ({
 
   return (
     <>
-      <div className='min-h-[620px] h-full'>
+      <div>
         {videoError ? (
-          <div className='w-full h-full bg-grid-brightRed  border-red border-t-2 border-x-2 border-opacity-60 p-2 border-b-0 shadow-red-blur flex justify-center items-center text-teal-blur text-2xl font-orbitron font-semibold'>
+          <div className='w-full h-[560px] bg-grid-brightRed  border-red border-2 border-opacity-60 shadow-red-blur flex justify-center items-center text-teal-blur text-2xl font-orbitron font-semibold'>
             Audio Error. Please refresh the page or visit another section.
           </div>
         ) : (
@@ -135,19 +135,21 @@ const VideoFrame = ({
           </div>
         )}
       </div>
-      <div className='h-2 border-b-2 bg-grid-darkRed border-l-2 border-red border-opacity-60 mr-2 relative pb-2 '>
-        <div className='absolute -right-3'>
-          <svg width='16' height='10' viewBox='0 0 16 10'>
-            <RedCRTBlur />
-            <g filter='url(#RedCRTBlur1) url(#RedCRTBlur2)'>
-              <path d='M0 0H12V1L3 10H0V0Z' fill={BLACK} />
-              <path d='M0 0H12V1L3 10H0V0Z' fill={RED_BLACK} fillOpacity='0.1' />
-              <path d='M0 0H12V1L3 10H0V0Z' fill='url(#redTile)' fillOpacity='0.85' />
-              <path d='M12 1L3 10H0V8H2L9.92339 0H12V1Z' fill={RED} fillOpacity={0.6} />
-            </g>
-          </svg>
+      {videoError && (
+        <div className='h-2 border-b-2 bg-grid-darkRed border-l-2 border-red border-opacity-60 mr-2 relative pb-2 '>
+          <div className='absolute -right-3'>
+            <svg width='16' height='10' viewBox='0 0 16 10'>
+              <RedCRTBlur />
+              <g filter='url(#RedCRTBlur1) url(#RedCRTBlur2)'>
+                <path d='M0 0H12V1L3 10H0V0Z' fill={BLACK} />
+                <path d='M0 0H12V1L3 10H0V0Z' fill={RED_BLACK} fillOpacity='0.1' />
+                <path d='M0 0H12V1L3 10H0V0Z' fill='url(#redTile)' fillOpacity='0.85' />
+                <path d='M12 1L3 10H0V8H2L9.92339 0H12V1Z' fill={RED} fillOpacity={0.6} />
+              </g>
+            </svg>
+          </div>
         </div>
-      </div>
+      )}
     </>
   )
 }
