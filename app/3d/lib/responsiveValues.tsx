4567af198cc24dc7ac5af039617fbddd2ca11919
Adrian Pikios <adrian@evokelabs.com>
2024-03-18 20:11:21 +1100

    Dynamic responsive const entered

--- app/3d/lib/responsiveValues.tsx
+++ app/3d/lib/responsiveValues.tsx
@@ -142,12 +142,72 @@ export const ASPECT_RATIO_OFFSET: AspectRatioOffsetType = {
   ]
 }
 
-export const CAMERA_X_OFFSET: Record<string, number> = {
+export const CAMERA_X_OFFSET_RESPONSIVE: Record<string, number> = {
   BASE: 2.73,
-  SM: 2.72,
+  SM: 2.73,
   MD: 2.9,
   LG: 2.9,
   XL: 2.8,
   '2XL': 2.8,
   '3XL': 2.7
+}
+
+export const CAMERA_X_OFFSET_BASE_RESPONSIVE: Record<string, number> = {
+  BASE: 0.83,
+  SM: 0.825,
+  MD: 0.25,
+  LG: 0.075,
+  XL: 0.15,
+  '2XL': 0.2,
+  '3XL': 0
+}
+
+export const CAMERA_Y_OFFSET_RESPONSIVE: Record<string, number> = {
+  BASE: -1.25,
+  SM: -1.15,
+  MD: 0,
+  LG: 0,
+  XL: 0.2,
+  '2XL': 0.3,
+  '3XL': 0.3
+}
+
+export const X_POSITION_VIEWPORT_RESPONSIVE: Record<string, number> = {
+  BASE: 1,
+  SM: 1,
+  MD: 7,
+  LG: 2,
+  XL: 1,
+  '2XL': 1,
+  '3XL': 1
+}
+
+export const HTML_SCALE_RESPONSIVE: Record<string, number> = {
+  BASE: 0.032,
+  SM: 0.032,
+  MD: 0.032,
+  LG: 0.041,
+  XL: 0.038,
+  '2XL': 0.0345,
+  '3XL': 0.031
+}
+
+export const POSITION_REF_RESPONSIVE: Record<string, number[]> = {
+  BASE: [0.685, 0, 1.7],
+  SM: [0.68, 0, 1.7],
+  MD: [-0.06, 0, 1.9],
+  LG: [-0.3, 0, 1.9],
+  XL: [-0.1, 0, 1.9],
+  '2XL': [0, 0, 1.9],
+  '3XL': [-0.05, 0, 1.9]
+}
+
+export const INITIAL_TARGET_DESKTOP_RESPONSIVE: Record<string, number[]> = {
+  BASE: [0.183, 1.35, 2.1],
+  SM: [0.82, 1.39, 2.1], //or [0, 1.35, 2.2]
+  MD: [0.225, 1.39, 2.1],
+  LG: [0.07, 1.39, 2.1],
+  XL: [0.15, 1.39, 2.1],
+  '2XL': [0.28, 1.39, 2.1],
+  '3XL': [-0.05, 1.39, 2.1]
 }
