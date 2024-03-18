525b1559f508dfc4130dd91e46d95b10395e9a5e
Adrian Pikios <adrian@evokelabs.com>
2024-03-19 05:41:52 +1100

    CameraRig.tsx responsive dynamic - but BASE values is not right

--- app/3d/cameras/CameraRig.tsx
+++ app/3d/cameras/CameraRig.tsx
@@ -1,14 +1,23 @@
 import { MD_BREAKPOINT } from '@/app/libs/breakPoints'
+import { useScreenSize } from '@/app/libs/useScreenSize'
 import { useFrame, useThree } from '@react-three/fiber'
 import { PerspectiveCamera, Vector3 } from 'three'
+import {
+  CAMERA_X_OFFSET_BASE_RESPONSIVE,
+  CAMERA_X_OFFSET_RESPONSIVE,
+  CAMERA_Y_OFFSET_RESPONSIVE,
+  X_POSITION_VIEWPORT_RESPONSIVE
+} from '../lib/responsiveValues'
 
 const DEFAULT_CAMERA_DAMPING = 0.015
-const CAMERA_X_OFFSET = 2.7
-const CAMERA_X_OFFSET_BASE = -0
-const CAMERA_Y_OFFSET = 0.3
 
 const CameraRig = ({ fov, debug }: { fov: number; debug: boolean }) => {
   const { camera } = useThree()
+  const screenSize = useScreenSize()
+  const CAMERA_X_OFFSET = CAMERA_X_OFFSET_RESPONSIVE[screenSize]
+  const CAMERA_X_OFFSET_BASE = CAMERA_X_OFFSET_BASE_RESPONSIVE[screenSize]
+  const CAMERA_Y_OFFSET = CAMERA_Y_OFFSET_RESPONSIVE[screenSize]
+  const X_POSITION_VIEWPORT = X_POSITION_VIEWPORT_RESPONSIVE[screenSize]
   const CAMERA_DAMPING = window.innerWidth < MD_BREAKPOINT ? 0.7 : DEFAULT_CAMERA_DAMPING
 
   useFrame(({ pointer, viewport }) => {
@@ -19,7 +28,7 @@ const CameraRig = ({ fov, debug }: { fov: number; debug: boolean }) => {
     const pointerY = window.innerWidth < MD_BREAKPOINT ? 0 : pointer.y
 
     // Calculate the target position of the camera
-    const XPosition = CAMERA_X_OFFSET_BASE - (pointerX * (viewport.width / 1)) / CAMERA_X_OFFSET
+    const XPosition = CAMERA_X_OFFSET_BASE - (pointerX * (viewport.width / X_POSITION_VIEWPORT)) / CAMERA_X_OFFSET
     const YPosition = (CAMERA_X_OFFSET + pointerY) / 2
     const ZPosition = -CAMERA_Y_OFFSET
     const targetPosition = new Vector3(XPosition, YPosition, ZPosition)
