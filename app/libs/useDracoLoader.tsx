93054f65e26c4834ee6cd734c47f5e19a4caff78
Adrian Pikios <adrian@evokelabs.com>
2024-03-21 16:19:30 +1100

    Total Size progress bar

--- app/libs/useDracoLoader.tsx
+++ app/libs/useDracoLoader.tsx
@@ -1,12 +1,13 @@
+import { LoadingManager } from 'three'
 import { DRACOLoader, GLTFLoader } from 'three/examples/jsm/Addons.js'
 
 const DRACO_DECODER_PATH = 'https://www.gstatic.com/draco/v1/decoders/'
 
-export const useDracoLoader = () => {
+export const useDracoLoader = (manager?: LoadingManager | undefined) => {
   const dracoLoader = new DRACOLoader()
   dracoLoader.setDecoderPath(DRACO_DECODER_PATH)
 
-  const gltfLoader = new GLTFLoader()
+  const gltfLoader = new GLTFLoader(manager)
   gltfLoader.setDRACOLoader(dracoLoader)
 
   return gltfLoader
--- app/sections/Preloader.tsx
+++ app/sections/Preloader.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { LoadingManager } from 'three'
 import { SoundAudioLevelControls } from './data/types'
+import { useDracoLoader } from '../libs/useDracoLoader'
 
 const Preloader = ({
   setIsPreLoaderFinished,
@@ -8,11 +10,46 @@ const Preloader = ({
   setIsPreLoaderFinished: (value: boolean) => void
   soundAudioLevelControls: SoundAudioLevelControls
 }) => {
+  const [isModelLoaded, setIsModelLoaded] = useState(false)
+  const manager = new LoadingManager()
+  const loader = useDracoLoader(manager)
+  const [progress, setProgress] = useState(0)
+
+  useEffect(() => {
+    manager.onStart = (url, itemsLoaded, itemsTotal) => {
+      console.log('Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.')
+    }
+
+    manager.onLoad = () => {
+      console.log('Loading complete!')
+      setIsModelLoaded(true)
+    }
+
+    manager.onProgress = (url, itemsLoaded, itemsTotal) => {
+      console.log('Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.')
+
+      if (itemsLoaded > 0) {
+        const sizeOfCurrentItem = itemsLoaded / itemsTotal
+        const totalSize = sizeOfCurrentItem * itemsTotal
+        console.log('Estimated total size of GLB model: ' + totalSize + ' bytes')
+      }
+      const progress = (itemsLoaded / itemsTotal) * 100
+      setProgress(progress)
+    }
+
+    // Start loading the GLB file
+    // loader.load('/glb/JOI.glb', gltf => {
+    loader.load('/glb/EvokelabsRoom.glb', gltf => {
+      // Handle the loaded model here
+    })
+  }, [])
+
   return (
     <div className='w-full h-full absolute top-0 left-0 z-[10000000000000000] pointer-events-none'>
       <div className='flex h-full last:items-center justify-center'>
         <button
           className='pointer-events-auto'
+          disabled={!isModelLoaded}
           onClick={() => {
             soundAudioLevelControls.setMuteAll(false)
             soundAudioLevelControls.setMuteMusic(false)
@@ -26,6 +63,7 @@ const Preloader = ({
           ENTER
         </button>
       </div>
+      <div className='absolute bottom-0 left-0 bg-blue-500' style={{ width: `${progress}%` }} />
     </div>
   )
 }
