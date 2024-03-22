f2cb4e388aa67f2f70b6cde884695508f0fe9312
Adrian Pikios <adrian@evokelabs.com>
2024-03-22 18:39:47 +1100

    customhook for preloader mask

--- app/3d/EvokeLabs3D.tsx
+++ app/3d/EvokeLabs3D.tsx
@@ -55,6 +55,7 @@ import { ROUTE_CONFIG } from '../libs/ROUTE_CONFIG'
 import Preloader from '../sections/Preloader'
 
 import { gsap } from 'gsap'
+import { usePreloaderMasking } from './lib/usePreloaderMasking'
 
 // Constants
 const INITIAL_CAMERA_POSITION = [-0.3, 1.5, -1] as const
@@ -139,58 +140,8 @@ const Evokelabs3D = ({ router }: { router: NextRouter }) => {
   // GPU Hook
   const { lowGPU, setLowGPU } = useGPU()
 
-
-  useEffect(() => {
-    console.log('currentRouteSelection', currentRouteSelection)
-    if (isPreLoaderFinished && currentRouteSelection === null) {
-      const maskAnimation = gsap.fromTo(
-        '.masked-element',
-        {
-          webkitMaskSize: '686px 735px',
-          maskSize: '686px 735px',
-          webkitMaskPosition: '50% 40%', // center the mask
-          maskPosition: '50% 40%' // center the mask
-        },
-        {
-          duration: 5, // adjust duration as needed
-          webkitMaskSize: '1100% 1100%',
-          maskSize: '1100% 1100%',
-          ease: 'linear',
-          yoyo: true,
-          tween: 'circ.in',
-          delay: 4.5
-        }
-      )
-
-      // Remove the mask after 8 seconds or if currentRouteSelection changes
-      const timeoutId = setTimeout(() => {
-        const element = document.querySelector('.masked-element')
-        if (element) {
-          element.classList.add('unmasked')
-        }
-      }, 9000)
-
-      return () => {
-        // If the component unmounts or currentRouteSelection changes, remove the mask and stop the animation
-        clearTimeout(timeoutId)
-        maskAnimation.kill()
-        const element = document.querySelector('.masked-element')
-        if (element) {
-          element.classList.add('unmasked')
-        }
-      }
-    }
-  }, [isPreLoaderFinished, currentRouteSelection])
-
-  useEffect(() => {
-    if (currentRouteSelection !== null) {
-      const element = document.querySelector('.masked-element')
-      if (element) {
-        element.classList.add('unmasked')
-      }
-    }
-  }, [currentRouteSelection])
-
+  // Preloader Masking Hook
+  usePreloaderMasking(isPreLoaderFinished, currentRouteSelection)
   return (
     <>
       <SoundControlContext.Provider
---
+++ app/3d/lib/usePreloaderMasking.tsx
@@ -0,0 +1,55 @@
+import { useEffect } from 'react'
+import gsap from 'gsap'
+
+export const usePreloaderMasking = (isPreLoaderFinished: unknown, currentRouteSelection: unknown) => {
+  useEffect(() => {
+    console.log('currentRouteSelection', currentRouteSelection)
+    if (isPreLoaderFinished && currentRouteSelection === null) {
+      const maskAnimation = gsap.fromTo(
+        '.masked-element',
+        {
+          webkitMaskSize: '686px 735px',
+          maskSize: '686px 735px',
+          webkitMaskPosition: '50% 40%', // center the mask
+          maskPosition: '50% 40%' // center the mask
+        },
+        {
+          duration: 5, // adjust duration as needed
+          webkitMaskSize: '1100% 1100%',
+          maskSize: '1100% 1100%',
+          ease: 'linear',
+          yoyo: true,
+          tween: 'circ.in',
+          delay: 4.5
+        }
+      )
+
+      // Remove the mask after 8 seconds or if currentRouteSelection changes
+      const timeoutId = setTimeout(() => {
+        const element = document.querySelector('.masked-element')
+        if (element) {
+          element.classList.add('unmasked')
+        }
+      }, 9000)
+
+      return () => {
+        // If the component unmounts or currentRouteSelection changes, remove the mask and stop the animation
+        clearTimeout(timeoutId)
+        maskAnimation.kill()
+        const element = document.querySelector('.masked-element')
+        if (element) {
+          element.classList.add('unmasked')
+        }
+      }
+    }
+  }, [isPreLoaderFinished, currentRouteSelection])
+
+  useEffect(() => {
+    if (currentRouteSelection !== null) {
+      const element = document.querySelector('.masked-element')
+      if (element) {
+        element.classList.add('unmasked')
+      }
+    }
+  }, [currentRouteSelection])
+}
