715a6a57fa7f560430afef63ea98ac4a411b84f5
Adrian Pikios <adrian@evokelabs.com>
2024-03-25 20:29:31 +1100

    Add volume and delay state variables to CyberpunkAmbienceSoundControl

--- app/audio/ambienceSFX/CyberpunkAmbienceSoundControl.tsx
+++ app/audio/ambienceSFX/CyberpunkAmbienceSoundControl.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import useAudio from '../libs/useAudio'
 import { SoundControlContext } from '@/app/libs/SoundControlContext'
 
@@ -9,8 +9,14 @@ const TRANSITION_DURATION = 2500
 const LOOP = true
 const CyberpunkAmbienceSoundControl = () => {
   const { muteSFX } = useContext(SoundControlContext)
-  const VOLUME = muteSFX ? 0 : VOLUME_SET
-  const DELAY = muteSFX ? 0 : DELAY_SET
+  const [VOLUME, setVolume] = useState(muteSFX ? 0 : VOLUME_SET)
+  const [DELAY, setDelay] = useState(muteSFX ? 0 : DELAY_SET)
+
+  useEffect(() => {
+    setVolume(muteSFX ? 0 : VOLUME_SET)
+    setDelay(muteSFX ? 0 : DELAY_SET)
+  }, [muteSFX])
+
   useAudio(AUDIO_SOURCE, VOLUME, DELAY, TRANSITION_DURATION, LOOP)
   return null
 }
