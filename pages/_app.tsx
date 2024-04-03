548f74a686b80d6ed1c193568e864937201c6f83
Adrian Pikios <adrian@evokelabs.com>
2024-04-04 04:00:10 +1100

    SEO + meta image update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,10 @@ import Evokelabs3D from '@/app/3d/EvokeLabs3D'
 import '@/app/globals.css'
 
 import { useRouter } from 'next/router'
-import Link from 'next/link'
-import MainMenu from '@/app/sections/MainMenu'
-import SocialIcons from '@/app/ui/SocialIcons'
 
-export const metadata: Metadata = {
-  title: 'EVOKE LABS | Creative Technologist',
-  description: 'Welcome to Evoke Labs: the portfolio and business hub of Adrian Pikios, a Sydney-based freelance creative technologist.'
+const metadata: Metadata = {
+  title: 'EVOKE LABS || Freelance Creative Technologist',
+  description: 'Welcome to Evoke Labs: the online hub of Adrian Pikios, a Sydney-based freelance creative technologist.'
 }
 
 export default function RootLayout() {
@@ -22,6 +19,17 @@ export default function RootLayout() {
       <Head>
         <title>{String(metadata.title)}</title>
         <meta name='description' content={String(metadata.description)} />
+        <meta name='keywords' content='Evoke Labs, Creative Technologist, Adrian Pikios, Freelance' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta property='og:title' content={String(metadata.title)} />
+        <meta property='og:description' content={String(metadata.description)} />
+        <meta property='og:url' content='https://evokelabs.com/' />
+        <meta property='og:image' content='https://evokelabs.com/meta-cover-image.jpg' />
+        <meta name='twitter:card' content='summary' />
+        <meta name='twitter:title' content={String(metadata.title)} />
+        <meta name='twitter:description' content={String(metadata.description)} />
+        <meta name='twitter:image' content='https://evokelabs.com/meta-cover-image.jpg' />
+        <link rel='canonical' href='https://evokelabs.com/' />
       </Head>
       <Evokelabs3D router={router} />
     </>
