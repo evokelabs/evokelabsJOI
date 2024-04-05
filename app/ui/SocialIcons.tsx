7d0abd7a5d47afd6c012a3ee01e65b8524f069e4
Adrian Pikios <adrian@evokelabs.com>
2024-04-05 14:08:51 +1100

    Update GitHub SVG in SocialIcons component

--- app/ui/SocialIcons.tsx
+++ app/ui/SocialIcons.tsx
@@ -10,7 +10,7 @@ const SocialIcons = () => {
   return (
     <div className='absolute top-0 right-0 pt-3 2xl:pt-5 xl:pr-1 flex flex-row z-[10000000000000000] 2xl:scale-100 lg:scale-[70%] md:scale-65 scale-50 origin-top-right'>
       <Link href='https://github.com/evokelabs' target='_blank'>
-        <ButtonSocial SVGIcon={props => <GithubSVG {...props} />} />
+        <ButtonSocial SVGIcon={props => <GithubSVG />} />
       </Link>
       <Link href='https://www.linkedin.com/in/adrianpikios' target='_blank'>
         <ButtonSocial SVGIcon={props => <LinkedInSVG {...props} />} />
