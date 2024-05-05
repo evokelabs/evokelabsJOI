90e88086c34e074c5265b175f82615d7b05d7a7f
Adrian Pikios <adrian@evokelabs.com>
2024-05-05 22:38:29 +1000

    fix: github icon hover color

--- app/ui/SocialIcons.tsx
+++ app/ui/SocialIcons.tsx
@@ -18,7 +18,7 @@ const SocialIcons = ({ soundAudioLevelControls }: { soundAudioLevelControls: Sou
   return (
     <div className='absolute top-0 right-0 pt-3 2xl:pt-5 xl:pr-1 flex flex-row z-[10000000000000000] 2xl:scale-100 lg:scale-[70%] md:scale-65 scale-50 origin-top-right'>
       <Link href='https://github.com/evokelabs' target='_blank' onClick={handleLinkClick}>
-        <ButtonSocial SVGIcon={props => <GithubSVG />} />
+        <ButtonSocial SVGIcon={props => <GithubSVG {...props} />} />
       </Link>
       <Link href='https://www.linkedin.com/in/adrianpikios' target='_blank' onClick={handleLinkClick}>
         <ButtonSocial SVGIcon={props => <LinkedInSVG {...props} />} />
