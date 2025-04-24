397e9ef093da0bb928554766ca0576fb2218a822
Adrian Pikios <adrian@evokelabs.com>
2025-04-24 18:10:07 +1000

    refactor: Removed twitter references

--- app/ui/SocialIcons.tsx
+++ app/ui/SocialIcons.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ButtonSocial from './ButtonSocial'
 import GithubSVG from './svg/button/GithubSVG'
 import YouTubeSVG from './svg/button/YouTubeSVG'
-import TwitterSVG from './svg/button/TwitterSVG'
 import LinkedInSVG from './svg/button/LinkedInSVG'
 import Link from 'next/link'
 import useSoundControl from '../libs/useSoundControl'
@@ -17,17 +16,23 @@ const SocialIcons = ({ soundAudioLevelControls }: { soundAudioLevelControls: Sou
 
   return (
     <div className='absolute top-0 right-0 pt-3 2xl:pt-5 xl:pr-1 flex flex-row z-[10000000000000000] 2xl:scale-100 lg:scale-[70%] md:scale-65 scale-50 origin-top-right'>
-      <Link href='https://github.com/evokelabs' target='_blank' onClick={handleLinkClick}>
-        <ButtonSocial SVGIcon={props => <GithubSVG {...props} />} />
+      <Link
+        href='https://github.com/evokelabs'
+        target='_blank'
+        onClick={handleLinkClick}>
+        <ButtonSocial SVGIcon={(props) => <GithubSVG {...props} />} />
       </Link>
-      <Link href='https://www.linkedin.com/in/adrianpikios' target='_blank' onClick={handleLinkClick}>
-        <ButtonSocial SVGIcon={props => <LinkedInSVG {...props} />} />
+      <Link
+        href='https://www.linkedin.com/in/adrianpikios'
+        target='_blank'
+        onClick={handleLinkClick}>
+        <ButtonSocial SVGIcon={(props) => <LinkedInSVG {...props} />} />
       </Link>
-      <Link href='https://twitter.com/evokelabs' target='_blank' onClick={handleLinkClick}>
-        <ButtonSocial SVGIcon={props => <TwitterSVG {...props} />} />
-      </Link>
-      <Link href='https://www.youtube.com/evokelabs' target='_blank' onClick={handleLinkClick}>
-        <ButtonSocial SVGIcon={props => <YouTubeSVG {...props} />} />
+      <Link
+        href='https://www.youtube.com/evokelabs'
+        target='_blank'
+        onClick={handleLinkClick}>
+        <ButtonSocial SVGIcon={(props) => <YouTubeSVG {...props} />} />
       </Link>
     </div>
   )
